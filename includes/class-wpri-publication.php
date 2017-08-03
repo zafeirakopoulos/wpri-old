6eaf4f1327d88668f468ba1483bf53be1d60fe3a
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-08-03 15:46:53 +0300

    Add the file

---
+++ includes/class-wpri-publication.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Functionality about publication management.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    wpri
+ * @subpackage wpri/includes
+ */
+
+/**
+ * Functionality about publication management.
+ *
+ * Functionality about publication management.
+ *
+ * @since      1.0.0
+ * @package    wpri
+ * @subpackage wpri/includes
+ * @author     Zafeirakis Zafeirakopoulos
+ */
+class WPRI_Project {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+ 
+}
