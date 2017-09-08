1bc79504acd56253e83817e0190c5562943f27fa
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-08 15:06:01 +0300

    faculty template update

--- public/templates/faculty.php
+++ public/templates/faculty.php
@@ -1,9 +1,10 @@
-/**
+<?php /**
  * Template Name: faculty
  * Template Post Type: page
  * 
  * Template Description...
  **/
+?>
  
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
