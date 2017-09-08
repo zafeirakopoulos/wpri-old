df42374644b1e1c65342bf8f80fa1a51eb5722d2
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-08 15:07:50 +0300

    faculty template update

--- public/templates/faculty.php
+++ public/templates/faculty.php
@@ -4,17 +4,31 @@
  * 
  * Template Description...
  **/
-?>
  
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
- 		// Start the loop.
-  Works
  
+get_header(); ?>
 
-	</main><!-- .site-main -->
+<div id="main-content" class="main-content">
 
- 
-</div><!-- .content-area -->
- 
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
 
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				endwhile;
+			?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php
+ 
+get_footer();
