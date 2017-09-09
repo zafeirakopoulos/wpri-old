b68df00f87742f85e73cf2ac22293360ffd22931
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 15:19:21 +0300

    bootstrap for faculty template

--- public/templates/faculty.php
+++ public/templates/faculty.php
@@ -22,7 +22,7 @@ get_header(); ?>
 					// Start the Loop.
 					$member_table_name = $GLOBALS['wpdb']->prefix . "wpri_member" ;
 					$members = $GLOBALS['wpdb']->get_results("SELECT * FROM " . $member_table_name );
-					echo "<div class='grid-col' >";
+					echo "<div class='container' >";
   	    			echo "<ul>";
                     $usermeta_table = $GLOBALS['wpdb']->prefix . "usermeta";
                     $user_table = $GLOBALS['wpdb']->prefix . "users";
@@ -37,7 +37,7 @@ get_header(); ?>
                         $titlen= $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT meta_value FROM " . $usermeta_table . " WHERE meta_key='title' AND user_id = %d", $member->user));
                         $atitle = $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT name FROM " . $title_table ." WHERE id = %d", $titlen));
 
-	                    echo "<li style='border-radius: 25px;border: 2px solid #73AD21; padding: 20px;width:30%'>";
+	                    echo "<li class="faculty-thumb col-md-3 ">";
                         echo "<table>";
                         echo "<tr><h3 class='faculty'>";
                         echo $atitle." ".$fname." ".$lname;
