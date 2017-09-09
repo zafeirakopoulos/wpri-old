a742af7c0b4bbd3ab848ad0bde63d821a25ba8b7
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 10:20:12 +0300

    div instead of ul in faculty template

--- public/templates/faculty.php
+++ public/templates/faculty.php
@@ -23,7 +23,6 @@ get_header(); ?>
 					$member_table_name = $GLOBALS['wpdb']->prefix . "wpri_member" ;
 					$members = $GLOBALS['wpdb']->get_results("SELECT * FROM " . $member_table_name );
 					echo "<div class='grid-col' >";
-  	    			echo "<ul>";
                     $usermeta_table = $GLOBALS['wpdb']->prefix . "usermeta";
                     $user_table = $GLOBALS['wpdb']->prefix . "users";
                     $position_table = $GLOBALS['wpdb']->prefix . "wpri_position";
@@ -37,7 +36,7 @@ get_header(); ?>
                         $titlen= $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT meta_value FROM " . $usermeta_table . " WHERE meta_key='title' AND user_id = %d", $member->user));
                         $atitle = $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT name FROM " . $title_table ." WHERE id = %d", $titlen));
 
-	                    echo "<li style='border-radius: 25px;border: 2px solid #73AD21; padding: 20px;width:30%;'>";
+	                    echo "<div'>";
                         echo "<table>";
                         echo "<tr><h3 class='faculty'>";
                         echo $atitle." ".$fname." ".$lname;
@@ -51,9 +50,8 @@ get_header(); ?>
                         echo $email."<br>";
                         echo "</p></td></tr>";
                         echo "</table>";
-                        echo "</li><br>";
+                        echo "</div>";
 					}
-  	    			echo "</ul>";
 					echo "</div>";
 				?>
 			</div><!-- #faculty -->
