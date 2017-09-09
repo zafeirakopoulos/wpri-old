4396f21f57889ef83a081763b9af927868d22a1c
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 20:42:32 +0300

    typo

--- public/templates/faculty.php
+++ public/templates/faculty.php
@@ -6,7 +6,7 @@
  **/
 
 /* Choose the header from the plugin */
-/* instantiate in main, dont do it every time *//
+/* instantiate in main, dont do it every time */
 $template_loader = new WPRI_Template_Loader;
 $template_loader->get_template_part( 'bte', 'header' );
 
