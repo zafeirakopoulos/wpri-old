4eb23e713e882ad800028a4c8871fe94c472808f
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 20:33:49 +0300

    remove form top level

--- templates/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div class="col-sm-3 col-sm-offset-1">
-	<div class="sidebar-module">
-		<h4>News</h4>
-		<ol class="list-unstyled">
-			<?php wp_get_archives(  ); ?>
-		</ol>
-	</div>
-</div><!-- /.blog-sidebar -->
