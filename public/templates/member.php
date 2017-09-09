18195d09f808b3b19c5af90251c872fe406304c4
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 15:45:15 +0300

    member template

---
+++ public/templates/member.php
@@ -0,0 +1,41 @@
+<?php /**
+ * Template Name: faculty
+ * Template Post Type: page
+ *
+ * Display a list of all faculty members of the institute
+ **/
+
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<div id="member" >
+				<?php
+					$current_language = "en";
+
+					$member_table_name = $GLOBALS['wpdb']->prefix . "wpri_member" ;
+					$usermeta_table = $GLOBALS['wpdb']->prefix . "usermeta";
+					$user_table = $GLOBALS['wpdb']->prefix . "users";
+					$position_table = $GLOBALS['wpdb']->prefix . "wpri_position";
+					$title_table = $GLOBALS['wpdb']->prefix . "wpri_title";
+
+					$member_id=$_POST['id'];
+					$member = $GLOBALS['wpdb']->get_row($GLOBALS['wpdb']->prepare("SELECT * FROM " . $member_table_name . " WHERE id = %d", $member_id));
+					$user_id = $member->user;
+					$user = $GLOBALS['wpdb']->get_row($GLOBALS['wpdb']->prepare("SELECT * FROM " . $user_table . " WHERE ID = %d", $user_id));
+					$usermeta = $GLOBALS['wpdb']->get_results($GLOBALS['wpdb']->prepare("SELECT * FROM " . $usermeta_table . " WHERE user_id = %d", $user_id));
+				?>
+
+				<h1> <?php echo 'Faculty' ?> </h1>
+ 
+			</div><!-- #faculty -->
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php
+
+get_footer();
