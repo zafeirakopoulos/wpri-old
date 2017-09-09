6ad9dc021d46d2b828d1fba46f59d7ceb08b2b70
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 19:47:31 +0300

    add templates in top level

---
+++ templates/footer-bte.php
@@ -0,0 +1,28 @@
+</div> <!-- /.container -->
+
+    <footer class="bte-footer">
+        <div class="row">
+
+        	<div class="col-sm-2 col-sm-offset-4">
+                <img height="100px" src="<?php echo get_bloginfo( 'template_directory' );?>/gtu-white.png">
+            </div>
+    		<div class="col-sm-2">
+                <br>
+                <p>
+                    Gebze Technical University <br>
+                    Computer Engineering Building <br>
+                    Gebze – Kocaeli – Turkey <br>
+                    +90 (262) 605 2203 <br>
+                    bte {at} gtu.edu.tr
+                </p>
+            </div>
+
+       </div>
+
+       <link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
+
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
