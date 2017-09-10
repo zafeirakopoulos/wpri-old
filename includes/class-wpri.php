96e14ef9337f8d34c843ada1c27bb20a07213879
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-10 11:42:50 +0300

    make index into home, because I cannot change the index template

--- includes/class-wpri.php
+++ includes/class-wpri.php
@@ -236,6 +236,7 @@ class WPRI {
 		$this->loader->add_filter( 'template_include',$plugin_public, 'project_page_template', 99 );
 		$this->loader->add_filter( 'template_include',$plugin_public, 'projects_page_template', 99 );
 		$this->loader->add_filter( 'template_include',$plugin_public, 'home_page_template', 99 );
+		$this->loader->add_filter( 'template_include',$plugin_public, 'page_template', 99 );
 
 		#$this->wpri_load_templates();
 
