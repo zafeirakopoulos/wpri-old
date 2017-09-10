09273707681004032486bc00557574959ccacdcc
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-10 17:55:34 +0300

    add ajax

--- includes/class-wpri.php
+++ includes/class-wpri.php
@@ -226,6 +226,10 @@ class WPRI {
 
 		$plugin_public = new WPRI_Public( $this->get_plugin_name(), $this->get_version() );
 
+		$this->loader->	add_action( 'wp_ajax_change_locale', $plugin_public, 'wpri_ajax_change_locale' );
+		$this->loader->	add_action( 'wp_ajax_nopriv_change_locale', $plugin_public, 'wpri_ajax_change_locale' );
+
+
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
@@ -236,9 +240,6 @@ class WPRI {
 		$this->loader->add_action('init', $plugin_public,'WPRIStartSession', 1);
 		$this->loader->add_action('wp_logout', $plugin_public,'WPRIEndSession');
 		$this->loader->add_action('wp_login',$plugin_public, 'WPRIEndSession');
-
-		$this->loader->	add_action( 'wp_ajax_change_locale', $plugin_public, 'wpri_ajax_change_locale' );
-		$this->loader->	add_action( 'wp_ajax_nopriv_change_locale', $plugin_public, 'wpri_ajax_change_locale' );
 
 
 
