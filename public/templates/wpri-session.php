0eee1352ecc984daee232a8e27d3e521104b1dae
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-10 18:45:44 +0300

    add ajax

--- public/templates/wpri-session.php
+++ public/templates/wpri-session.php
@@ -5,7 +5,7 @@ nocache_headers();
 // ONLY USE A VARIABLE WITHIN $_SESSION (here, "ajjx")
 // OTHERWISE THIS MAY ALLOW ANYONE TO TAKE CONTROL OF YOUR INSTALLATION.
 // Check if locale is set
-$_SESSION['locale'] = $_REQUEST['locale'];
+$_SESSION['locale'] = $_REQUEST['data'];
 
 echo $_SESSION['locale'] ;
 /*
