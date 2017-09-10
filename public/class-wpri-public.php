e0a7b430cc7ee8f34bf2f274ff419f4f90f89753
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-10 17:45:01 +0300

    add ajax

--- public/class-wpri-public.php
+++ public/class-wpri-public.php
@@ -142,7 +142,7 @@ class WPRI_Public {
 	public function WPRIEndSession() {
 		session_destroy ();
 	}
-
+/*
 	function wpri_ajax_change_locale() {
 		// Handle request then generate response using WP_Ajax_Response
 		$_SESSION['locale'] = "locale changed";
@@ -150,4 +150,5 @@ class WPRI_Public {
 		// Don't forget to stop execution afterward.
 		wp_die();
 	}
+	*/
 }
