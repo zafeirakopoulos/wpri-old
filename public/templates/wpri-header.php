a99652bbd00efe7227e54730a250f2d5c3baf455
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-10 16:22:55 +0300

    add sessions

--- public/templates/wpri-header.php
+++ public/templates/wpri-header.php
@@ -56,8 +56,10 @@
 					  <li><a href="<?php echo get_permalink( get_page_by_path('contact'))?>">Contact</a></li>
 			      </ul>
 			      <ul class="nav navbar-nav navbar-right">
+					  <li><p class="navbar-btn"> <a href="#" class="btn btn-default">I'm a link button!</a></p></li>
 	<li><a href='<?php echo home_url(add_query_arg(array(),$wp->request))."?locale=En_US";?>'>English</a></li>
 	<li><a href='<?php echo home_url(add_query_arg(array(),$wp->request))."?locale=Tr_TR";?>'>Turkish</a></li>
+	$_SESSION['myKey'] = "Some data I need later";
 			      </ul>
 			    </div><!--/.nav-collapse -->
 			  </div><!--/.container-fluid -->
