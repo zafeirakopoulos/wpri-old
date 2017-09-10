25e43327858c182501edd0a301eb91d8cc63b12f
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-10 18:21:39 +0300

    add ajax

--- public/templates/wpri-session.php
+++ public/templates/wpri-session.php
@@ -7,10 +7,10 @@ nocache_headers();
 // Check if locale is set
 $_SESSION['locale'] = $_POST['locale'];
 
-/*
+
 Header('Content-Type: application/json;charset=utf8');
 die(json_encode(array(
     'result' => 'OK', // This in case you want to return something to the caller
 )));
-*/
+
  ?>
