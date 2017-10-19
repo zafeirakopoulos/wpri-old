22af46d987d9d03fdf97f0159c11cbbdd3f29514
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-19 22:53:41 +0300

    debug

--- includes/class-wpri-pages.php
+++ includes/class-wpri-pages.php
@@ -32,7 +32,7 @@ class WPRI_Pages {
 	 * @since    1.0.0
 	 */
 
-/*
+
 	public static function the_slug_exists($post_name) {
 		global $wpdb;
 		if($wpdb->get_row("SELECT post_name FROM wp_posts WHERE post_name = '" . $post_name . "'", 'ARRAY_A')) {
@@ -41,7 +41,7 @@ class WPRI_Pages {
 			return false;
 		}
 	}
-*/
+
 	public static function create_pages() {
 
 		$pages_list = array('People','Research','Projects','Publications','Contact');
@@ -56,8 +56,8 @@ class WPRI_Pages {
 				    'post_author' => 1,
 				    'post_slug' => strtolower($page_title)
 			    );
-			 //   if(!isset($page_check->ID) && !WPRI_Pages::the_slug_exists(strtolower($page_title))){
-			 	if(!isset($page_check->ID) && !WPRI_Pages::the_slug_exists(strtolower($page_title))){
+			    if(!isset($page_check->ID) && !WPRI_Pages::the_slug_exists(strtolower($page_title))){
+			 	//if(!isset($page_check->ID) && !WPRI_Pages::the_slug_exists(strtolower($page_title))){
        				$page_id = wp_insert_post($page);
 			    }
 			//}
