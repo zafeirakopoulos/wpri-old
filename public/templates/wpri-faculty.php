4854c122f102de14565dbbf88371500f5de8f7f9
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-20 22:44:12 +0300

    debug

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -10,21 +10,21 @@
 	    				foreach ( $member_ids as $member_id ) {
 							$member = WPRI_Database::get_member($member_id, $locale);
 
-		   					//echo "<a href='".site_url()."/member?id=".$member->user."'><div class='faculty-thumb col-md-5'>";
-							// echo "<table>";
-							// echo "<tr><h3 class='faculty'>";
-							// echo $atitle." ".$fname." ".$lname;
-							// echo "</h3></tr>";
-							// echo "<tr><td>";
-							// echo get_avatar( $member->user );
-							// echo "</td>";
-							// echo "<td><p>";
-							// echo $position."<br>";
-							// echo $member->website."<br>";
-							// echo $email."<br>";
-							// echo "</p></td></tr>";
-							// echo "</table>";
-							//echo "</div></a>";
+		   					echo "<a href='".site_url()."/member?id=".$member->user."'><div class='faculty-thumb col-md-5'>";
+							echo "<table>";
+							echo "<tr><h3 class='faculty'>";
+							echo $atitle." ".$fname." ".$lname;
+							echo "</h3></tr>";
+							echo "<tr><td>";
+							echo get_avatar( $member->user );
+							echo "</td>";
+							echo "<td><p>";
+							echo $position."<br>";
+							echo $member->website."<br>";
+							echo $email."<br>";
+							echo "</p></td></tr>";
+							echo "</table>";
+							echo "</div></a>";
 					}
 					echo "</div>";
 				?>
