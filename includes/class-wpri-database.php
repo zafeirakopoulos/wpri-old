076f4acaf5e70062db0dbb7d429d24757085d313
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-20 22:31:35 +0300

    debug

--- includes/class-wpri-database.php
+++ includes/class-wpri-database.php
@@ -489,9 +489,10 @@ class WPRI_Database {
 			// This should return full information, gathered from different tables
 			public static function get_member($member_id, $locale) {
 				echo "test";
+				echo $member_id;
 				$member_data = $GLOBALS['wpdb']->query(
 					$GLOBALS['wpdb']->prepare(
-						"SELECT * FROM " . self::table_name("member"). " WHERE id = %d", (string)$member_id
+						"SELECT * FROM " . self::table_name("member"). " WHERE id = %d", $member_id
 					)
 				);
 
