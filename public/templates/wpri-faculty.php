a181af5cdb2339b736756a5d36da4207966ffb82
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-20 22:33:50 +0300

    debug

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -5,6 +5,7 @@
 
 					// Start the Loop.
 					$member_ids = WPRI_Database::get_member_ids();
+					echo $member_ids;
 					$locale = 1; // TODO: read from the session
 					echo "<div class='container' >";
 	    				foreach ( $member_ids as $member_id ) {
