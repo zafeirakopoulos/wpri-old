d5e12f03bda79ab961d7f89f0cfab67f6df04169
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-21 22:25:10 +0300

    debug

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -1,33 +1,30 @@
 
 			<div id="projects" >
-				<h1> <?php echo 'Projects' ?> </h1>
+				<h1> Projects </h1>
 				<?php
-					$current_language = "en";
-					// This has to be done using js. Cannot be written in DB for example.
-					// Have a hidden element in the DOM to keep track of language.
 
 					// Start the Loop.
-					$projects = get_wpri_projects();
+					$project_ids = WPRI_Database::get_project_ids();
+					$locale = 1; // TODO: read from the session
 					echo "<div class='container' >";
-
-    				foreach ( $projects as $project) {
-
-						echo "<a href='".site_url()."/member?id=".$member->user."'><div class='faculty-thumb col-md-5'>";
-                        echo "<table>";
-                        echo "<tr><h3 class='faculty'>";
-                        echo $atitle." ".$fname." ".$lname;
-                        echo "</h3></tr>";
-                        echo "<tr><td>";
-                        echo get_avatar( $member->user );
-                        echo "</td>";
-                        echo "<td><p>";
-                        echo $position."<br>";
-                        echo $member->website."<br>";
-                        echo $email."<br>";
-                        echo "</p></td></tr>";
-                        echo "</table>";
-                        echo "</div></a>";
+	    				foreach ( $member_ids as $member_id ) {
+							$member = WPRI_Database::get_member_short($member_id);
+		   					echo "<a href='".site_url()."/member?id=".$member_id."'><div class='faculty-thumb col-md-5'>";
+							echo "<table>";
+							echo "<tr><h3 class='faculty'>";
+							echo $member['title']." ".$member['name'];
+							echo "</h3></tr>";
+							echo "<tr><td>";
+							echo get_avatar($member['user'] );
+							echo "</td>";
+							echo "<td><p>";
+							echo $member['position']."<br>";
+							echo $member['website']."<br>";
+							echo $member['email']."<br>";
+							echo "</p></td></tr>";
+							echo "</table>";
+							echo "</div></a>";
 					}
 					echo "</div>";
 				?>
-			</div><!-- #projects -->
+			</div><!-- #faculty -->
