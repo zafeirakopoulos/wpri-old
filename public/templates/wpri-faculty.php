9ebb64de1d0ba767731d672f41e04f63990b555f
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-21 15:27:49 +0300

    debug

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -9,20 +9,18 @@
 					echo "<div class='container' >";
 	    				foreach ( $member_ids as $member_id ) {
 							$member = WPRI_Database::get_member_short($member_id);
-							echo $member;
-							echo $member['user'];
-		   					echo "<a href='".site_url()."/member?id=".$member->user."'><div class='faculty-thumb col-md-5'>";
+		   					echo "<a href='".site_url()."/member?id=".$member['user']."'><div class='faculty-thumb col-md-5'>";
 							echo "<table>";
 							echo "<tr><h3 class='faculty'>";
-							echo $member->title." ".$member->name;
+							echo $member['title']." ".$member['name'];
 							echo "</h3></tr>";
 							echo "<tr><td>";
-							echo get_avatar( $member->user );
+							echo get_avatar($member['user'] );
 							echo "</td>";
 							echo "<td><p>";
-							echo $member->position."<br>";
-							echo $member->website."<br>";
-							echo $member->email."<br>";
+							echo $member['position']."<br>";
+							echo $member['website']."<br>";
+							echo $member['email']."<br>";
 							echo "</p></td></tr>";
 							echo "</table>";
 							echo "</div></a>";
