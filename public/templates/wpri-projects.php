1ad57d3fb680640217422ccfa55aad5469d86ba7
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-21 22:28:01 +0300

    debug

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -5,24 +5,15 @@
 
 					// Start the Loop.
 					$project_ids = WPRI_Database::get_project_ids();
-					$locale = 1; // TODO: read from the session
 					echo "<div class='container' >";
-	    				foreach ( $member_ids as $member_id ) {
-							$member = WPRI_Database::get_member_short($member_id);
-		   					echo "<a href='".site_url()."/member?id=".$member_id."'><div class='faculty-thumb col-md-5'>";
-							echo "<table>";
+	    				foreach ( $project_ids as $project_id ) {
+							$project = WPRI_Database::get_project_short($project_id);
+		   					echo "<a href='".site_url()."project?id=".$project_id."'><div class='faculty-thumb col-md-5'>";
 							echo "<tr><h3 class='faculty'>";
-							echo $member['title']." ".$member['name'];
-							echo "</h3></tr>";
-							echo "<tr><td>";
-							echo get_avatar($member['user'] );
-							echo "</td>";
-							echo "<td><p>";
+							echo $project['title'];
 							echo $member['position']."<br>";
 							echo $member['website']."<br>";
 							echo $member['email']."<br>";
-							echo "</p></td></tr>";
-							echo "</table>";
 							echo "</div></a>";
 					}
 					echo "</div>";
