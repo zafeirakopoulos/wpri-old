c1ac3e9914f67c24345209332ac7228ece3ea3dd
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-21 14:35:39 +0300

    debug

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -1,6 +1,6 @@
 
 			<div id="faculty" >
-				<h1> <?php echo 'Faculty' ?> </h1>
+				<h1> Faculty </h1>
 				<?php
 
 					// Start the Loop.
@@ -8,20 +8,20 @@
 					$locale = 1; // TODO: read from the session
 					echo "<div class='container' >";
 	    				foreach ( $member_ids as $member_id ) {
-							$member = WPRI_Database::get_member($member_id, $locale);
+							$member = WPRI_Database::get_member($member_id);
 
 		   					echo "<a href='".site_url()."/member?id=".$member->user."'><div class='faculty-thumb col-md-5'>";
 							echo "<table>";
 							echo "<tr><h3 class='faculty'>";
-							echo $atitle." ".$fname." ".$lname;
+							echo $member->title." ".$member->name;
 							echo "</h3></tr>";
 							echo "<tr><td>";
 							echo get_avatar( $member->user );
 							echo "</td>";
 							echo "<td><p>";
-							echo $position."<br>";
+							echo $member->position."<br>";
 							echo $member->website."<br>";
-							echo $email."<br>";
+							echo $member->email."<br>";
 							echo "</p></td></tr>";
 							echo "</table>";
 							echo "</div></a>";
