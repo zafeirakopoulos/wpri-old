2818894ec85ce9223694dbe6aa1ac075631ba0e5
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-21 18:32:16 +0300

    debug

--- includes/class-wpri-database.php
+++ includes/class-wpri-database.php
@@ -313,7 +313,7 @@ class WPRI_Database {
 
 				public static function get_roles() {
 					$locale=1;
-					
+
 	 		 		return  $GLOBALS['wpdb']->get_results(
 						$GLOBALS['wpdb']->prepare(
 							"SELECT * FROM " . self::table_name("locale_role"). " WHERE locale= %d",
@@ -322,6 +322,15 @@ class WPRI_Database {
 					);
 				}
 
+				public static function get_titles() {
+					$locale=1;
+	 		 		return  $GLOBALS['wpdb']->get_results(
+						$GLOBALS['wpdb']->prepare(
+							"SELECT * FROM " . self::table_name("locale_title"). " WHERE locale= %d",
+							$locale
+						)
+					);
+				}
 				///////////////////////////
 				// Member
 				///////////////////////////
