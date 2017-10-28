c054ce74b4bf6f2ad907816d178eca468dc433e7
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-28 23:23:20 +0300

    css

--- public/templates/page.php
+++ public/templates/page.php
@@ -3,8 +3,8 @@ $template_loader = new WPRI_Template_Loader;
 $template_loader->get_template_part( 'wpri', 'header' );
 ?>
 <!-- <div class="container" style="background-image: url('http://bte.gtu.edu.tr/wp-content/uploads/2016/03/bg2.jpg'); "> -->
-<div class="container">
-    <div class="row" width="100%">
+<div class="container" width="100%">>
+    <div class="row"
         <div class="col-sm-12 col-lg-10">
             <?php
             if ( is_page( 'faculty' ) ) {
