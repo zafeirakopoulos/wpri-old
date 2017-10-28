2a7b9bb2c64be58ea4305c36341d2d773b0dc64d
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-28 17:29:21 +0300

    debug

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -8,8 +8,8 @@
 					echo "<div class='container' >";
 	    				foreach ( $project_ids as $project_id ) {
 							$project = WPRI_Database::get_project_short($project_id);
-		   					echo "<a href='".site_url()."/project?id=".$project_id."'><div class='faculty-thumb col-md-5'>";
-							echo "<tr><h3 class='faculty'>";
+		   					echo "<a href='".site_url()."/project?id=".$project_id."'><div class='project-thumb col-md-10'>";
+							echo "<tr><h3 class='project-thumb'>";
 							echo $project['title'];
 							echo $project['locale_description']."<br>";
 							echo $project['status']."<br>";
