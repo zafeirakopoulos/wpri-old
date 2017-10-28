6c5561d4952aadc1a8a013380f71ba5b5c577a40
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-28 22:40:16 +0300

    css

--- public/templates/sidebar-news.php
+++ public/templates/sidebar-news.php
@@ -5,15 +5,6 @@
 		<ol class="list-unstyled">
 
 
-
-			<?php 			$args = array( 'post_type' => 'news', 'posts_per_page' => 10 );
-						$loop = new WP_Query( $args );
-						while ( $loop->have_posts() ) : $loop->the_post();
-						  the_title();
-						  echo '<div class="entry-content">';
-						  the_content();
-						  echo '</div>';
-						endwhile;?>
 		</ol>
 	</div>
 </div><!-- /.blog-sidebar -->
