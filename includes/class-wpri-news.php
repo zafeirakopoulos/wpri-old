588109a207d911a4607c35e18396c52ad8fc4eee
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-28 21:29:56 +0300

    css

---
+++ includes/class-wpri-news.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Functions managing the news posts.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    wpri
+ * @subpackage wpri/includes
+ */
+
+/**
+ * Functions managing the news posts.
+ *
+ * Functions managing the news posts.
+ *
+ * @since      1.0.0
+ * @package    wpri
+ * @subpackage wpri/includes
+ * @author     Zafeirakis Zafeirakopoulos
+ */
+class WPRI_News {
+
+
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+
+
+	public static function create_news_posttype() {
+		  register_post_type( 'wpri_news',
+		    array(
+		      'labels' => array(
+		        'name' => __( 'News' ),
+		        'singular_name' => __( 'News' )
+		      ),
+		      'public' => true,
+		      'has_archive' => true,
+		      'rewrite' => array('slug' => 'news'),
+		    )
+		  );
+	  }
+
+
+}
