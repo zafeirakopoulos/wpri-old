0ce05e75c2df63d1bdd93b72c9e7d1d9d53b323d
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 00:39:43 +0300

    css

--- public/templates/wpri-single.php
+++ public/templates/wpri-single.php
@@ -1,11 +1,23 @@
 
 			<div class='single'  >
-  dd rebre
 				<?php
 
 				$post = get_post(get_the_ID());
-				foreach ($post as $key => $value) {
-					echo $key." = ".$value;
+				if ($post->type=="wpri-news"){
+
 				}
+				else {
+					foreach ($post as $key => $value) {
+						echo $key." = ".$value;
+					}
+				}
+
 				?>
+				<div class="previous-post-link">
+                <?php previous_post_link('%link', '<< Previous Post', $in_same_term = true, $excluded_terms = '', $taxonomy = 'the-custom-taxonomy-associated-with-your-custom-post-type'); ?>
+	            </div>
+
+	            <div class="next-post-link">
+	                <?php next_post_link('%link', 'Next Post >>', $in_same_term = true, $excluded_terms = '', $taxonomy = 'the-custom-taxonomy-associated-with-your-custom-post-type'); ?>
+	            </div>
 			</div>
