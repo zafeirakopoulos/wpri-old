584a21da65b8f3358fdd3c1e50bc57015f6736f0
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-28 20:39:30 +0300

    css

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -5,11 +5,11 @@
 
 					// Start the Loop.
 					$member_ids = WPRI_Database::get_member_ids();
-					echo "<div class='container faculty-thumb>";
+					echo "<div class='container>";
 	    				foreach ( $member_ids as $member_id ) {
 							$member = WPRI_Database::get_member_short($member_id);
 		   					echo "<a href='".site_url()."/member?id=".$member_id."'>";
-							echo "<div class='col-md-5'>";
+							echo "<div class='col-md-5  faculty-thumb'>";
 							echo "<table>";
 							echo "<tr><h3 class='faculty-thumb'>";
 							echo $member['title']." ".$member['name'];
