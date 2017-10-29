147ceba13752712122df05a8b96feb53cef46c54
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 11:57:27 +0300

    css

--- public/templates/wpri-member.php
+++ public/templates/wpri-member.php
@@ -3,37 +3,41 @@
 				<?php
 					$member_id=$_GET['id'];
 					$member = WPRI_Database::get_member($member_id);
+				?>
+				<br>
+				<table>
+				<tr><h3>
+				<?php$member['title']." ".$member['name'];?>
+				</h3></tr>
+				<tr><td>
+					<?php get_avatar( $member['user'] );?>
+				</td>
+				<td><p>
+					<?php $member['position'];?>
+				<br>
+					<?php $member['website'];?>
+				<br>
+					<?php $member['email'];?>
+				<br>
+				</p></td></tr>
+				</table>
 
-					echo "<br>";
-					echo "<table>";
-					echo "<tr><h3>";
-					echo $member['title']." ".$member['name'];
-					echo "</h3></tr>";
-					echo "<tr><td>";
-					echo get_avatar( $member['user'] );
-					echo "</td>";
-					echo "<td><p>";
-					echo $member['position']."<br>";
-					echo $member['website']."<br>";
-					echo $member['email']."<br>";
-					echo "</p></td></tr>";
-					echo "</table>";
-
-					echo "<h2>Publications:</h2><br>";
-					echo "<table>";
-					$member_publications = WPRI_Database::get_member_publications($member_id);
+				<h2>Publications:</h2><br>
+					<table>
+					<?php $member_publications = WPRI_Database::get_member_publications($member_id);
 					foreach ($member_publications as $publication) {
 						echo "<tr>";
 						$pub = WPRI_Database::get_publication($publication->pub);
 						echo "<td><a href='".site_url()."/publication?id=".$pub->id."'>" . $pub->title."</a><td>";
 						echo "</tr>";
 					}
-					echo "</table>";
+					?>
+					</table>
 
 
-					echo "<h2>Projects:</h2><br>";
-					echo "<table>";
-					foreach ($member['projects'] as $project_member) {
+				<h2>Projects:</h2><br>
+				<table>
+				<?php foreach ($member['projects'] as $project_member) {
 						$project = WPRI_Database::get_project( $project_member->project);
 						echo "<tr>";
 		 				echo "<td>" . $project->title . "<td>";
@@ -41,18 +45,18 @@
 		 				echo "<td>" . $project->PI . "<td>";
 		 				echo "<td>" . $project->funding . "<td>";
 						echo "</tr>";
-					}
-					echo "</table>";
+					}?>
+				</table>
 
-					echo "<h2>Education:</h2><br>";
+				<h2>Education:</h2><br>
 
- 						$args = array(
+				<h2>News:</h2><br>
+
+				<?php $args = array(
 						  'author__in'     => array($member['user']),
 						  'posts_per_page' => 20,
 						  'post_type' => 'wpri_news'
 						);
-
-					echo "<h2> Projects:</h2><br>";
 
 						$loop = new WP_Query( $args );
 						while ( $loop->have_posts() ) : $loop->the_post();
