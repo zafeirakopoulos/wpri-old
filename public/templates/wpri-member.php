6dc6679c59a00dde078bc3321baa3ef9be2db0a8
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 12:22:49 +0300

    css

--- public/templates/wpri-member.php
+++ public/templates/wpri-member.php
@@ -58,7 +58,7 @@
 
 						$loop = new WP_Query( $args );
 						while ( $loop->have_posts() ) : $loop->the_post();
-							echo "<a class='member' href='".site_url()."/news/".$post->post_name."'>";
+							echo "<br><a class='member' href='".site_url()."/news/".$post->post_name."'>";
 							the_title();
 							echo "</a>";
 						endwhile;
