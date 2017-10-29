bb8055bd4a60b2869c9492833c0f11008b0e9523
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 13:08:35 +0300

    css

--- public/templates/wpri-member.php
+++ public/templates/wpri-member.php
@@ -59,7 +59,7 @@
 						);
 
 						$loop = new WP_Query( $args );
-						echo "<dev class=çontainer'>";
+						echo "<dev class='container'>";
 						while ( $loop->have_posts() ) : $loop->the_post();
 							echo "<a class='member' href='".site_url()."/news/".$post->post_name."'>";
 								echo "<div class='row'>";
