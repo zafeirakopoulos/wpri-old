f74f18b9e8f5f1e5b98bc5eac52128f06f744640
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 21:44:43 +0300

    css

--- public/templates/wpri-positions.php
+++ public/templates/wpri-positions.php
@@ -1,44 +1,20 @@
-
-			<div id="faculty" >
-				<h1> <?php echo 'Faculty' ?> </h1>
-				<?php
-					$current_language = "en";
-					// This has to be done using js. Cannot be written in DB for example.
-					// Have a hidden element in the DOM to keep track of language.
-
-					// Start the Loop.
-					$member_table_name = $GLOBALS['wpdb']->prefix . "wpri_member" ;
-					$members = $GLOBALS['wpdb']->get_results("SELECT * FROM " . $member_table_name );
-					echo "<div class='container' >";
-                    $usermeta_table = $GLOBALS['wpdb']->prefix . "usermeta";
-                    $user_table = $GLOBALS['wpdb']->prefix . "users";
-                    $position_table = $GLOBALS['wpdb']->prefix . "wpri_position";
-                    $title_table = $GLOBALS['wpdb']->prefix . "wpri_title";
-
-    				foreach ( $members as $member ) {
-                        $fname = $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT meta_value FROM " . $usermeta_table . " WHERE meta_key='first_name' AND user_id = %d", $member->user));
-                        $lname = $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT meta_value FROM " . $usermeta_table . " WHERE meta_key='last_name' AND user_id = %d", $member->user));
-                        $email = $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT user_email FROM " . $user_table . " WHERE ID = %d", $member->user));
-                        $position = $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT name FROM " . $position_table ." WHERE id = %d", $member->position));
-                        $titlen= $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT meta_value FROM " . $usermeta_table . " WHERE meta_key='title' AND user_id = %d", $member->user));
-                        $atitle = $GLOBALS['wpdb']->get_var($GLOBALS['wpdb']->prepare("SELECT name FROM " . $title_table ." WHERE id = %d", $titlen));
-
-						echo "<a href='".site_url()."/member?id=".$member->user."'><div class='faculty-thumb col-md-5'>";
-                        echo "<table>";
-                        echo "<tr><h3 class='faculty'>";
-                        echo $atitle." ".$fname." ".$lname;
-                        echo "</h3></tr>";
-                        echo "<tr><td>";
-                        echo get_avatar( $member->user );
-                        echo "</td>";
-                        echo "<td><p>";
-                        echo $position."<br>";
-                        echo $member->website."<br>";
-                        echo $email."<br>";
-                        echo "</p></td></tr>";
-                        echo "</table>";
-                        echo "</div></a>";
-					}
+<div class='faculty' id="positions" >
+	<div class='container'>
+ 		<div class='row'>
+			<?php
+			// Start the Loop.
+			$position_ids = WPRI_Database::get_open_position_ids();
+			foreach ( $position_ids as $position_id ) {
+				$position = WPRI_Database::get_open_position($position_id);
+				echo "<a class='faculty-thumb' href='".site_url()."/position?id=".$position_id."'>";
+					echo "<div class='col-sm-12 col-md-5 col-lg-5 faculty-thumb-frame'>";
+						echo "<div class='col-sm-12 col-md-12 col-lg-12 faculty-thumb'><h1 class='faculty'>".$position['title']."</h1> </div>";
+						echo "<div class='col-sm-6 col-md-6 col-lg-6 faculty-thumb'>Deadline</div>";
+						echo "<div class='col-sm-6 col-md-6 col-lg-6 faculty-thumb'>".$position['deadline']."</div>";
 					echo "</div>";
-				?>
-			</div><!-- #faculty -->
+				echo "</a>";
+			}
+ 			?>
+		</div>
+	</div>
+ </div><!-- #faculty -->
