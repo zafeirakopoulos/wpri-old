9e72be5b1162ae6fbde49b226d5eb6490a8dd03b
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 20:33:29 +0300

    css

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -1,23 +1,20 @@
+<div class='faculty' id="faculty" >
+	<div class='container'>
+ 		<div class='row'>
+			<?php
+			// Start the Loop.
+			foreach ( $project_ids as $project_id ) {
+				$project = WPRI_Database::get_project_short($project_id);
+				echo "<a class='project-thumb' href='".site_url()."/project?id=".$project_id."'>";
 
-			<div id="projects" >
-				<h1> Projects </h1>
-				<?php
-
-					// Start the Loop.
-					$project_ids = WPRI_Database::get_project_ids();
-					echo "<div class='container' >";
-	    				foreach ( $project_ids as $project_id ) {
-							$project = WPRI_Database::get_project_short($project_id);
-		   					echo "<a href='".site_url()."/project?id=".$project_id."'><div class='project-thumb col-md-10'>";
-							echo "<h3 class='project-thumb'>";
-							echo $project['title'];
-							echo "</h3>";
-							echo "<p>";
-							echo $project['locale_description']."<br>";
-							echo $project['status']."<br>";
-							echo "</p>";
-							echo "</div></a>";
-					}
+					echo "<div class='col-sm-12 col-md-5 col-lg-5 faculty-thumb-frame'>";
+						echo "<div class='col-sm-12 col-md-12 col-lg-12 faculty-thumb'><h1 class='faculty'>".$project['title']."</h1> </div>";
+						echo "<div class='col-sm-3 col-md-3 col-lg-3 faculty-thumb'>".$project['status']."</div>";
+						echo "<div class='col-sm-9 col-md-9 col-lg-9 faculty-thumb'>".$project['PI']."</div>";
 					echo "</div>";
-				?>
-			</div><!-- #faculty -->
+				echo "</a>";
+			}
+ 			?>
+		</div>
+	</div>
+</div><!-- #projects-->
