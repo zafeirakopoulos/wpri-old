a480825d12807d504e798225635408a651be28b5
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 21:55:13 +0300

    css

--- public/templates/wpri-positions.php
+++ public/templates/wpri-positions.php
@@ -7,10 +7,10 @@
 			foreach ( $position_ids as $position_id ) {
 				$position = WPRI_Database::get_open_position($position_id);
 				echo "<a class='faculty-thumb' href='".site_url()."/position?id=".$position_id."'>";
-					echo "<div class='col-sm-12 col-md-5 col-lg-5 faculty-thumb-frame'>";
+					echo "<div class='col-sm-12 col-md-12 col-lg-12 faculty-thumb-frame'>";
 						echo "<div class='col-sm-12 col-md-12 col-lg-12 faculty-thumb'><h1 class='faculty'>".$position['title']."</h1> </div>";
-						echo "<div class='col-sm-6 col-md-6 col-lg-6 faculty-thumb'>Deadline</div>";
-						echo "<div class='col-sm-6 col-md-6 col-lg-6 faculty-thumb'>".$position['deadline']."</div>";
+						echo "<div class='col-sm-3 col-md-3 col-lg-3 faculty-thumb'>Deadline</div>";
+						echo "<div class='col-sm-9 col-md-9 col-lg-9 faculty-thumb'>".$position['deadline']."</div>";
 					echo "</div>";
 				echo "</a>";
 			}
