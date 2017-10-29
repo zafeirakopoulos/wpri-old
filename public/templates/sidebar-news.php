59a7fc03a9dca3c8e196df5b1c691bd74949bb2a
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 11:38:57 +0300

    css

--- public/templates/sidebar-news.php
+++ public/templates/sidebar-news.php
@@ -1,16 +1,18 @@
-<div class="wpri-sidebar">
-	<h4 class="wpri-sidebar">News</h4>
-	<ol class="wpri-sidebar">
-		<?php
-			$args = array( 'post_type' => 'wpri_news', 'posts_per_page' => 10 );
-			$loop = new WP_Query( $args );
-			while ( $loop->have_posts() ) : $loop->the_post();
-				echo "<a class='faculty-thumb-link' href='".site_url()."/news/".$post->post_name."'>";
-					echo '<div>';
-					the_title();
-	  				echo '</div>';
-				echo "</a>";
-			endwhile;
-		?>
-	</ol>
+<div id="wpri-sidebar" >
+	<h1 class='wpri-sidebar'> News </h1>
+	<div class="wpri-sidebar">
+		<ol class="wpri-sidebar">
+			<?php
+				$args = array( 'post_type' => 'wpri_news', 'posts_per_page' => 10 );
+				$loop = new WP_Query( $args );
+				while ( $loop->have_posts() ) : $loop->the_post();
+					echo "<a class='faculty-thumb-link' href='".site_url()."/news/".$post->post_name."'>";
+						echo '<div>';
+						the_title();
+		  				echo '</div>';
+					echo "</a>";
+				endwhile;
+			?>
+		</ol>
+	</div>
 </div>
