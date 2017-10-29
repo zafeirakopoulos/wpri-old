c2457b08c3c08727f06c772703d0e1589e40e094
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 19:25:08 +0300

    css

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -9,7 +9,7 @@
 				$member_id=$_GET['id'];
 				echo $member_id;
 				$member = WPRI_Database::get_member($member_id);
-				echo "<a class='faculty-thumb' href='".site_url()."/member?id=".$member_id."'>";
+				echo "<a class='faculty-thumb-frame' href='".site_url()."/member?id=".$member_id."'>";
 						echo "<div class='col-sm-12 col-md-5 col-lg-5'>";
 							echo "<div class='col-sm-12 col-md-12 col-lg-12 faculty-thumb'><h1 class='faculty'>".$member['title']." ".$member['name']."</h1> </div>";
 							echo "<div class='col-sm-3 col-md-3 col-lg-3 faculty-thumb'>".get_avatar( $member['user'])."</div>";
