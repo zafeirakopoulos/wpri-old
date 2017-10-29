5fbaa5109d90b02738ebe8f3b0c8cd8d599f7dc8
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 20:49:38 +0300

    css

--- public/templates/wpri-publications.php
+++ public/templates/wpri-publications.php
@@ -1,18 +1,18 @@
-
-			<div id="publications" >
-				<h1> Publications </h1>
-				<?php
-
-					// Start the Loop.
-					$publication_ids = WPRI_Database::get_publication_ids();
-					echo "<div class='container' >";
-	    				foreach ( $publication_ids as $publication_id ) {
-							$publication = WPRI_Database::get_publication_short($publication_id);
-		   					echo "<a href='".site_url()."/publication?id=".$publication_id."'><div class='faculty-thumb col-md-5'>";
-							echo "<tr><h3 class='faculty'>";
-							echo $publication['title'];
-							echo "</div></a>";
-					}
+<div class='faculty' id="publications" >
+	<div class='container'>
+ 		<div class='row'>
+			<?php
+			// Start the Loop.
+			$publication_ids = WPRI_Database::get_publication_ids();
+			foreach ( $publication_ids as $publication_id ) {
+				$publication = WPRI_Database::get_publication_short($publication_id);
+				echo "<a class='faculty-thumb'  href='".site_url()."/publication?id=".$publication_id."'>";
+					echo "<div class='col-sm-12 col-md-5 col-lg-5 faculty-thumb-frame'>";
+						echo "<div class='col-sm-12 col-md-12 col-lg-12 faculty-thumb'><h1 class='faculty'>".$publication['title']."</h1> </div>";
 					echo "</div>";
-				?>
-			</div><!-- #faculty -->
+				echo "</a>";
+			}
+ 			?>
+		</div>
+	</div>
+</div><!-- #publications-->
