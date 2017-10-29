8c6f3ebce57af4302b6c08131d0b8c0a125c2730
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 23:58:50 +0300

    css

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -7,7 +7,7 @@
 			foreach ( $project_ids as $project_id ) {
 				$project = WPRI_Database::get_project_short($project_id);
 				echo "<a class='project-thumb' href='".site_url()."/project?id=".$project_id."'>";
-					echo "<div class='col-sm-12 col-md-6 col-lg-6 faculty-thumb-frame'>";
+					echo "<div class='col-sm-12 col-md-5 col-lg-5 faculty-thumb-frame'>";
 						echo "<div class='col-sm-12 col-md-12 col-lg-12 faculty-thumb'><h1 class='faculty-thumb'>".$project['title']."</h1> </div>";
 						echo "<div class='col-sm-3 col-md-3 col-lg-3 faculty-thumb'>".$project['status']."</div>";
 						echo "<div class='col-sm-9 col-md-9 col-lg-9 faculty-thumb'>".$project['PI']."</div>";
