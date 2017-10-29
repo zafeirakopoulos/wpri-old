2c404aa916ab90f464b20ac329fde67f62a5fdee
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 21:27:26 +0300

    css

--- public/templates/wpri-contact.php
+++ public/templates/wpri-contact.php
@@ -1,5 +1,17 @@
-
-			<div id="projects" >
-				<h1> <?php echo 'Contact' ?> </h1>
-
-			</div><!-- #projects -->
+<div class="member">
+	<div class="container">
+		Address:
+		<div class='row'>
+			<div class='col-sm-3 col-md-3 col-lg-3'>Gebze Technical University Computer Engineering Building </div>
+			<div class='col-sm-3 col-md-3 col-lg-3'>Gebze – Kocaeli – Turkey </div>
+			<div class='col-sm-3 col-md-3 col-lg-3'>ZIP Code : 41400 </div>
+		</div>
+		Other:
+		<div class='row'>
+			<div class='col-sm-3 col-md-3 col-lg-3'>Phone:</div>
+			<div class='col-sm-9 col-md-9 col-lg-9'>+90 (262) 605 2203</div>
+			<div class='col-sm-3 col-md-3 col-lg-3'>E-mail</div>
+			<div class='col-sm-9 col-md-9 col-lg-9'>bte {at} gtu.edu.tr</div>
+		</div>
+	</div>
+</div><!-- #contact -->
