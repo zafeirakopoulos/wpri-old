79cd7b0d09ec4a36b1302f8b70bc59d574cd50a8
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 20:45:18 +0300

    css

--- public/templates/wpri-project.php
+++ public/templates/wpri-project.php
@@ -1,36 +1,56 @@
 
-			<div class='project' id="project" >
-				<?php
-					$project_id=$_GET['id'];
-					$project = WPRI_Database::get_project_full($project_id);
+<div class="member">
+	<div class="container">
+		<?php
+		$project_id=$_GET['id'];
+		$project = WPRI_Database::get_project_full($project_id);
+		?>
+		<div class='row'>
+			<div class='col-sm-12 col-md-12 col-lg-12'><h1> <?php echo $project['title'];?></h1> </div>
+		</div>
+		<div class='row'>
+			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo "project picture"?> </div>
+			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo $member['position'];?> </div>
+		</div>
+		<div class='row'>
+			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo $project["funding"];?> </div>
+			<div class='col-sm-3 col-md-3 col-lg-3'> 	<?php echo $member['email'];?> </div>
+		</div>
+	</div>
 
-					echo "<br>";
-					echo "<tr><h3 class='project'>";
-					echo $project['title'];
-					echo $project['position']."<br>";
-					echo $project['website']."<br>";
-					echo $project['email']."<br>";
-					echo $project["funding"]."<br>";
 
-					echo "Participants:<br>";
-					echo "<table>";
-					$project_members = WPRI_Database::get_project_members($project_id);
-					foreach ($project_members as $member) {
-						echo "<tr>";
-		 				echo "<td>" . WPRI_Database::get_member($member->member)["name"]. "(". WPRI_Database::get_project_role($member->role).")<td>";
-						echo "</tr>";
-					}
-					echo "</table>";
+		<h2 class="member">Participants:</h2>
+		<div class="container">
+		<?php
+			$project_members = WPRI_Database::get_project_members($project_id);
+			foreach ($project_members as $member) {
+				echo "<a class='member' href='".site_url()."/member?id=".$member->id."'>";
+				echo "<div class='row'>";
+					echo "<div class='col-sm-12 col-md-12 col-lg-12'>".WPRI_Database::get_member($member->member)["name"]. "(". WPRI_Database::get_project_role($member->role).")</div>";
+				echo "</div>";
+				echo "</a>";
+			}?>
+		</div>
 
-					echo "Publications:<br>";
-					echo "<table>";
-					$project_publications = WPRI_Database::get_project_publications($project_id);
-					foreach ($project_publications as $publication) {
-						echo "<tr>";
-						$pub = WPRI_Database::get_publication($publication->pub);
-						echo "<td><a href='".site_url()."/publication?id=".$pub->id."'>" . $pub->title."</a><td>";
-						echo "</tr>";
-					}
-					echo "</table>";
-				?>
-			</div><!-- #member -->
+	<h2 class="member">Publications:</h2>
+	<div class="container">
+		<?php
+		$project_publications = WPRI_Database::get_project_publications($project_id);
+		foreach ($project_publications as $publication) {
+			$pub = WPRI_Database::get_publication($publication->pub);
+
+			echo "<a class='member' href='".site_url()."/publication?id=".$pub->id."'>";
+			echo "<div class='row'>";
+				echo "<div class='col-sm-12 col-md-12 col-lg-12'>".$pub->title."</div>";
+			echo "</div>";
+			echo "</a>";
+		}?>
+	</div>
+
+
+
+<!--
+<h2 class="member">News:</h2>
+Needs work. From project management connect a wpri_news post with the project to query from here.
+-->
+</div><!-- #project -->
