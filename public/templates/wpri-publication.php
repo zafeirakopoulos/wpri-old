4f30581c275df01780d6e994101ebb8e02fd421b
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-29 21:03:35 +0300

    css

--- public/templates/wpri-publication.php
+++ public/templates/wpri-publication.php
@@ -1,50 +1,48 @@
 
-			<div id="member" >
-				<?php
-					$member_id=$_GET['id'];
-					$member = WPRI_Database::get_member($member_id);
-
-					echo "<br>";
-					echo "<table>";
-					echo "<tr><h3 class='faculty'>";
-					echo $member['title']." ".$member['name'];
-					echo "</h3></tr>";
-					echo "<tr><td>";
-					echo get_avatar( $member['user'] );
-					echo "</td>";
-					echo "<td><p>";
-					echo $member['position']."<br>";
-					echo $member['website']."<br>";
-					echo $member['email']."<br>";
-					echo "</p></td></tr>";
-					echo "</table>";
-
-					echo "Publications:<br>";
-					echo "<table>";
-					$member_publications = WPRI_Database::get_member_publications($member_id);
-					foreach ($member_publications as $publication) {
-						echo "<tr>";
-						$pub = WPRI_Database::get_publication($publication->pub);
-						echo "<td><a href='".site_url()."/publication?id=".$pub->id."'>" . $pub->title."</a><td>";
-						echo "</tr>";
-					}
-					echo "</table>";
+<div class="member">
+	<div class="container">
+		<?php
+		$publication_id=$_GET['id'];
+		$publication = WPRI_Database::get_publication_full($publication_id);
+		?>
+		<div class='row'>
+			<div class='col-sm-12 col-md-12 col-lg-12'><h1> <?php echo $publication['title'];?></h1> </div>
+		</div>
+		<div class='row'>
+			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo "publication picture"?> </div>
+			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo $publication['doi'];?> </div>
+			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo $publication["pubtype"];?> </div>
+		</div>
+		<div class='row'>
+			<div class='col-sm-12 col-md-12 col-lg-12'> <?php echo $publication['bibentry'];?> </div>
+		</div>
+	</div>
 
 
-					echo "Projects:<br>";
-					echo "<table>";
-					foreach ($member['projects'] as $project_member) {
-						$project = WPRI_Database::get_project( $project_member->project);
-						echo "<tr>";
-		 				echo "<td>" . $project->title . "<td>";
-						echo "<td>" . WPRI_Database::get_project_role($project_member->role) . "<td>";
-		 				echo "<td>" . $project->PI . "<td>";
-		 				echo "<td>" . $project->funding . "<td>";
-						echo "</tr>";
-					}
-					echo "</table>";
 
-					echo "Education:<br>";
+	<h2 class="member">Authors:</h2>
+	<div class="container">
+	<?php
+		foreach ($publication['authors'] as $author) {
+			echo "<a class='member' href='".site_url()."/member?id=".$author->id."'>";
+			echo "<div class='row'>";
+				echo "<div class='col-sm-12 col-md-12 col-lg-12'>".$author->name."</div>";
+			echo "</div>";
+			echo "</a>";
+		}?>
+	</div>
 
-				?>
-			</div><!-- #member -->
+	<h2 class="member">Projects:</h2>
+	<div class="container">
+		<?php
+		foreach ($publication['projects'] as $project) {
+			echo "<a class='member' href='".site_url()."/project?id=".$project->id."'>";
+			echo "<div class='row'>";
+				echo "<div class='col-sm-12 col-md-12 col-lg-12'>".$project->title."</div>";
+			echo "</div>";
+			echo "</a>";
+		}?>
+	</div>
+
+
+</div><!-- #publication -->
