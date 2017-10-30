ee5e4daad0a514412e63095b18da7f1ce4fe70c3
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-31 00:26:16 +0300

    css

--- public/templates/wpri-positions.php
+++ public/templates/wpri-positions.php
@@ -7,9 +7,8 @@
 				$position = WPRI_Database::get_open_position($position_id);
 				echo "<div class='col-sm-12 listing-thumb-frame'>";
 					echo "<a class='listing-thumb' href='".site_url()."/position?id=".$position_id."'>";
-						echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>".get_avatar( $member['user'])."</div>";
 					echo "<div class='row'>";
-						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$position['title']."</h1> </div>";
+						echo "<div class='col-xs-12 col-md-12 col-ld-12 listing-thumb'><h1 class='listing'>".$position['title']."</h1> </div>";
 					echo "</div>";
 					echo "</a>";
 					echo "<div class='row'>";
