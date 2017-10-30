09505c4b1355306030be16350fae30b79edf6bea
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-31 00:11:04 +0300

    css

--- public/templates/wpri-project.php
+++ public/templates/wpri-project.php
@@ -1,20 +1,20 @@
 
-<div class="member">
+<div class="single">
 	<div class="container">
 		<?php
 		$project_id=$_GET['id'];
 		$project = WPRI_Database::get_project_full($project_id);
 		?>
 		<div class='row'>
-			<div class='col-sm-12 col-md-12 col-lg-12'><h1> <?php echo $project['title'];?></h1> </div>
+			<div class='col-sm-12 col-md-12 col-lg-12 single'><h1> <?php echo $project['title'];?></h1> </div>
 		</div>
 		<div class='row'>
-			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo "project picture"?> </div>
-			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo $member['position'];?> </div>
+			<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo "project picture"?> </div>
+			<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $member['position'];?> </div>
 		</div>
 		<div class='row'>
-			<div class='col-sm-3 col-md-3 col-lg-3'> <?php echo $project["funding"];?> </div>
-			<div class='col-sm-3 col-md-3 col-lg-3'> 	<?php echo $member['email'];?> </div>
+			<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $project["funding"];?> </div>
+			<div class='col-sm-3 col-md-3 col-lg-3 single'> 	<?php echo $member['email'];?> </div>
 		</div>
 	</div>
 
@@ -24,23 +24,23 @@
 	<?php
 		$project_members = WPRI_Database::get_project_members($project_id);
 		foreach ($project_members as $member) {
-			echo "<a class='member' href='".site_url()."/member?id=".$member->id."'>";
+			echo "<a class=' single' href='".site_url()."/member?id=".$member->id."'>";
 			echo "<div class='row'>";
-				echo "<div class='col-sm-12 col-md-12 col-lg-12'>".WPRI_Database::get_member($member->member)["name"]. "(". WPRI_Database::get_project_role($member->role).")</div>";
+				echo "<div class='col-sm-12 col-md-12 col-lg-12 single'>".WPRI_Database::get_member($member->member)["name"]. "(". WPRI_Database::get_project_role($member->role).")</div>";
 			echo "</div>";
 			echo "</a>";
 		}?>
 	</div>
 
-	<h2 class="member">Publications:</h2>
+	<h2 class=" single">Publications:</h2>
 	<div class="container">
 		<?php
 		$project_publications = WPRI_Database::get_project_publications($project_id);
 		foreach ($project_publications as $publication) {
 			$pub = WPRI_Database::get_publication($publication->pub);
-			echo "<a class='member' href='".site_url()."/publication?id=".$pub->id."'>";
+			echo "<a class=' single' href='".site_url()."/publication?id=".$pub->id."'>";
 			echo "<div class='row'>";
-				echo "<div class='col-sm-12 col-md-12 col-lg-12'>".$pub->title."</div>";
+				echo "<div class='col-sm-12 col-md-12 col-lg-12 single'>".$pub->title."</div>";
 			echo "</div>";
 			echo "</a>";
 		}?>
