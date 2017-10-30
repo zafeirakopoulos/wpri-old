88bf4232d58f9db7792408d2804af3e8a6aa7ce5
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-30 23:45:28 +0300

    css

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -5,7 +5,6 @@
 			$member_ids = WPRI_Database::get_member_ids();
 			foreach ( $member_ids as $member_id ) {
 				$member = WPRI_Database::get_member_short($member_id);
-
 				echo "<div class='col-sm-12 listing-thumb-frame'>";
 					echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member_id."'>";
 						echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>".get_avatar( $member['user'])."</div>";
@@ -13,7 +12,6 @@
 						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$member['title']." ".$member['name']."</h1> </div>";
 					echo "</div>";
 					echo "</a>";
-
 					echo "<div class='row'>";
 						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['position']."</div>";
 						if (isset($member['website']) AND $member['website']!=""){
