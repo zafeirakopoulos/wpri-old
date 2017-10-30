760df10ad63b0639646798ef33fc00409079acb7
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-30 19:26:02 +0300

    css

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -1,5 +1,5 @@
 <div class='faculty' id="faculty" >
-	<div class='container'>
+	<!-- <div class='container'> -->
  		<div class='row'>
 			<?php
 			// Start the Loop.
@@ -7,7 +7,8 @@
 			foreach ( $member_ids as $member_id ) {
 				$member = WPRI_Database::get_member_short($member_id);
 
-				echo "<div class='col-sm-12 row'>";
+				// echo "<div class='col-sm-12 row faculty-thumb-frame'>";
+				echo "<div class='col-sm-12 faculty-thumb-frame'>";
 					echo "<a class='faculty-thumb' href='".site_url()."/member?id=".$member_id."'>";
 						echo "<div class='col-xs-12 faculty-thumb'><h1 class='faculty'>".$member['title']." ".$member['name']."</h1> </div>";
 						echo "<div class='col-xs-3 faculty-thumb'>".get_avatar( $member['user'])."</div>";
@@ -19,5 +20,5 @@
 			}
  			?>
 		</div>
-	</div>
+	<!-- </div> -->
  </div><!-- #faculty -->
