58a8b615a2426afbecdb3a0ad25cdf30607b9f23
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-31 00:18:32 +0300

    css

--- public/templates/wpri-contact.php
+++ public/templates/wpri-contact.php
@@ -1,18 +1,18 @@
-<div class="member">
+<div class=" single">
 	<div class="container">
-		Address:
+		<h2 class=" single">Address:</h2>
 		<div class='row'>
-			<div class='col-sm-12 col-md-12 col-lg-12'>Gebze Technical University Computer Engineering Building </div>
-			<div class='col-sm-12 col-md-12 col-lg-12'>Gebze – Kocaeli – Turkey </div>
-			<div class='col-sm-12 col-md-12 col-lg-12'>ZIP Code : 41400 </div>
+			<div class='col-sm-12 col-md-12 col-lg-12 single'>Gebze Technical University Computer Engineering Building </div>
+			<div class='col-sm-12 col-md-12 col-lg-12 single'>Gebze – Kocaeli – Turkey </div>
+			<div class='col-sm-12 col-md-12 col-lg-12 single'>ZIP Code : 41400 </div>
 		</div>
 		<br><br>
 
 		<div class='row'>
-			<div class='col-sm-3 col-md-3 col-lg-3'>Phone:</div>
-			<div class='col-sm-9 col-md-9 col-lg-9'>+90 (262) 605 2203</div>
-			<div class='col-sm-3 col-md-3 col-lg-3'>E-mail</div>
-			<div class='col-sm-9 col-md-9 col-lg-9'>bte {at} gtu.edu.tr</div>
+			<div class='col-sm-3 col-md-3 col-lg-3 single'>Phone:</div>
+			<div class='col-sm-9 col-md-9 col-lg-9 single'>+90 (262) 605 2203</div>
+			<div class='col-sm-3 col-md-3 col-lg-3 single'>E-mail</div>
+			<div class='col-sm-9 col-md-9 col-lg-9 single'>bte {at} gtu.edu.tr</div>
 		</div>
 	</div>
 </div><!-- #contact -->
