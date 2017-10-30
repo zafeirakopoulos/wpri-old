f18470bb82b7056d8e0ef7739226887ef6628a1e
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-10-30 22:15:18 +0300

    css

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -8,15 +8,15 @@
 
 				echo "<div class='col-sm-12 faculty-thumb-frame'>";
 				echo "<a class='faculty-thumb' href='".site_url()."/member?id=".$member_id."'>";
-				echo "<div class='col-xs-3 faculty-thumb'>".get_avatar( $member['user'])."</div>";
+				echo "<div class='col-xs-12 col-md-6 col-ld-12  faculty-thumb'>".get_avatar( $member['user'])."</div>";
 				echo "</a>";
 				// echo "<div class='col-xs-9 faculty-thumb'>";
 					echo "<div class='row'>";
-					echo "<div class='col-xs-3 faculty-thumb'><h1 class='faculty'>".$member['title']." ".$member['name']."</h1> </div>";
+					echo "<div class='col-xs-12 col-md-6 col-ld-12 faculty-thumb'><h1 class='faculty'>".$member['title']." ".$member['name']."</h1> </div>";
 					echo "</div>";
-					echo "<div class='col-xs-3 faculty-thumb'>".$member['position']."</div>";
-					echo "<div class='col-xs-3 faculty-thumb'>".$member['website']."</div>";
-					echo "<div class='col-xs-3 faculty-thumb'>".$member['email']."</div>";
+					echo "<div class='col-xs-12 col-md-6 col-ld-12 faculty-thumb'>".$member['position']."</div>";
+					echo "<div class='col-xs-12 col-md-6 col-ld-12 faculty-thumb'>".$member['website']."</div>";
+					echo "<div class='col-xs-12 col-md-6 col-ld-12 faculty-thumb'>".$member['email']."</div>";
 				// echo "</div>";
 
 
