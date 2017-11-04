3c1ccb9dd4cdbf376d7d9c20a399078fba2f8b0e
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-04 18:50:39 +0300

    restructured. HTML forms in calls

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -42,14 +42,16 @@ class WPRI_Form {
 
 	   			<div class="tab-content clearfix">
 		   			<div class="tab-pane active" id="add">
-		            	<h3>Content's background color is the same for the tab</h3>
-		   			</div>
+						<?php
+					   WPRI_Form::wpri_form_add($form);
+					   ?>		   			</div>
 		   			<div class="tab-pane" id="update">
-		            	<h3>We use the class nav-pills instead of nav-tabs which automatically creates a background color for the tab</h3>
-		   			</div>
+						<?php
+					   WPRI_Form::wpri_form_update($form);
+					   ?>		   			</div>
 		            <div class="tab-pane" id="delete">
 						 <?php
-			 			WPRI_Form::wpri_form_from_array($form);
+			 			WPRI_Form::wpri_form_delete($form);
 			 			?>
 		   			</div>
 	   			</div>
@@ -57,7 +59,7 @@ class WPRI_Form {
 		<?php
 	}
 
-	public static function wpri_form_from_array($form) {
+	public static function wpri_form_add($form) {
 		?>
 		<div>
 			<div class='row'>
@@ -87,9 +89,94 @@ class WPRI_Form {
 					<?php } ?>
 					</div>
 				<?php } ?>
-				<?php foreach ($group["actions"] as $action) {?>
-					<button type="submit" name="action" value="<?php echo $action?>"> <?php echo $action?></button>
+				<button type="submit" name="action" value="add">Add</button>
+			</form>
+		</div>
+	<?php }
+
+
+	public static function wpri_form_delete($form) {
+		?>
+		<div class="tab-content clearfix">
+			<div class="tab-pane active" id="select-delete">
+				<?php
+				$all_entries = WPRI_Database::get_all($form["table_name"]);
+				?>
+				<form name='id' method="post" action="#confirm-delete">
+					<div class="list-group">
+						<?php foreach ( $all_entries as $item ) {?>
+					  	   <input type="submit" name="add_button" value="<?php $item->id ?>" class="button-secondary"/>
+						<?php } ?>
+					</div>
+					<input type="hidden" name="type" value="select"/>
+				</form>
+			</div>
+			<div class="tab-pane" id="confirm-delete">
+				<div class='row'>
+					<form name='<?php echo $form["name"]?>' method="post" action="">
+						<div class='col-sm-12 form-title'> <?php echo $form["title"] ?> </div>
+						<?php
+						foreach ($form["groups"] as $group) {?>
+							<hr/>
+							<div class='col-sm-12 form-group-title'> <?php echo $group["title"] ?> </div>
+							<?php
+							foreach ($group["elements"] as $element) {
+								if ($element["type"]=="text"){?>
+									<label class='col-sm-3 form-element-caption'> <?php echo $element["caption"] ?></label>
+									<div class='col-sm-3 form-element-value'>
+										<textarea id='<?php echo $element["id"]?>'
+												  name='<?php echo $element["name"]?>'
+												  cols='<?php echo $element["cols"]?>'
+												  rows='<?php echo $element["rows"]?>'>
+											 <?php echo $element["value"]?>
+										</textarea>
+									</div>
+								<?php }
+								elseif ($element["type"]=="radio"){?>
+									<div class='col-sm-3 form-element-caption'> <?php echo $element["caption"] ?></div>
+									<div class='col-sm-3 form-element-value'>   <?php echo $element["value"]   ?></div>
+								<?php }	?>
+							<?php } ?>
+						<?php } ?>
+						<button type="submit" name="action" value="add">Add</button>
+					</form>
+				</div>
+		   </div>
+		</div>
+	</div>
+	<?php }
+
+	public static function wpri_form_update($form) {
+		?>
+		<div>
+			<div class='row'>
+			<form name='<?php echo $form["name"]?>' method="post" action="">
+				<div class='col-sm-12 form-title'> <?php echo $form["title"] ?> </div>
+				<?php
+				foreach ($form["groups"] as $group) {?>
+					<hr/>
+					<div class='col-sm-12 form-group-title'> <?php echo $group["title"] ?> </div>
+					<?php
+					foreach ($group["elements"] as $element) {
+						if ($element["type"]=="text"){?>
+							<label class='col-sm-3 form-element-caption'> <?php echo $element["caption"] ?></label>
+							<div class='col-sm-3 form-element-value'>
+								<textarea id='<?php echo $element["id"]?>'
+										  name='<?php echo $element["name"]?>'
+										  cols='<?php echo $element["cols"]?>'
+										  rows='<?php echo $element["rows"]?>'>
+									 <?php echo $element["value"]?>
+								</textarea>
+							</div>
+						<?php }
+						elseif ($element["type"]=="radio"){?>
+							<div class='col-sm-3 form-element-caption'> <?php echo $element["caption"] ?></div>
+							<div class='col-sm-3 form-element-value'>   <?php echo $element["value"]   ?></div>
+						<?php }	?>
+					<?php } ?>
+					</div>
 				<?php } ?>
+				<button type="submit" name="action" value="add">Add</button>
 			</form>
 		</div>
 	<?php }
