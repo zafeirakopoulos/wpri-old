e2948fa9d621faccdd731db6cc86d91943d76bae
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-04 18:55:42 +0300

    restructured. HTML forms in calls

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -46,17 +46,17 @@ class WPRI_Form {
 					   WPRI_Form::wpri_form_add($form);
 					   ?>		   			</div>
 		   			<div class="tab-pane" id="update">
-						<!-- <?php
+					 <?php
 					   WPRI_Form::wpri_form_update($form);
-					   ?> -->
+					   ?>
 		   			</div>
-		            <!-- <div class="tab-pane" id="delete">
+		            <div class="tab-pane" id="delete">
 						 <?php
 			 			WPRI_Form::wpri_form_delete($form);
 			 			?>
-		   			</div> -->
-	   			</div>
-	     </div>
+		   			</div>
+			 	</div>
+			</div>
 		<?php
 	}
 
