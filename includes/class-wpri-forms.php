270003e45acd42dd1c65430bdb81b6a0edb85914
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-04 21:34:51 +0300

    restructured. HTML forms in calls

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -29,15 +29,17 @@ class WPRI_Form {
 	 *
 	 * @since    1.0.0
 	 */
-    public static function wpri_create_form($title, $form) {?>
+    public static function wpri_create_form($title, $form) {
+		// WPRI_Form::wpri_form_handle_request($form);
+		?>
 		<div class="container"><h1><?php echo $title ?> </h1></div>
 	    	<div id="exTab1" class="container">
 				<ul  class="nav nav-pills">
-					<li class="active"><a href="#add" data-toggle="tab">Add</a>
+					<li class="active"><a href="#add" data-toggle="tab">Add New</a>
 					</li>
-					<li><a href="#update" data-toggle="tab">Update</a>
+					<li><a href="#update" data-toggle="tab">Update Existing</a>
 					</li>
-					<li><a href="#delete" data-toggle="tab">Delete</a>
+					<li><a href="#delete" data-toggle="tab">Delete Existing</a>
 			   	</ul>
 
 	   			<div class="tab-content clearfix">
@@ -59,6 +61,56 @@ class WPRI_Form {
 			 	</div>
 			</div>
 		<?php
+	}
+
+	public static function wpri_form_handle_request($form) {
+
+
+		// If POST for adding
+		if( isset( $_POST['type']) && $_POST['type'] == 'add_' . $setting_name ) {
+
+			$GLOBALS['wpdb']->insert( $table_name , array( 'name' => $_POST["setting_name"] ) );
+			echo $GLOBALS['wpdb']->insert_id ;
+			$new_id = $GLOBALS['wpdb']->insert_id;
+			if($GLOBALS['wpdb']->insert_id) {
+				?>
+			<div class="updated"><p><strong>Added.</strong></p></div>
+				<?php
+			} else {
+				?>
+			<div class="error"><p>Unable to add.</p></div>
+			<?php
+			}
+			foreach ( $locales as $locale ) {
+				$GLOBALS['wpdb']->insert( $mixed_table_name , array(
+					'locale' => $locale->id,
+					$setting_name => $new_id,
+					'name' => $_POST["setting_name_" . $locale->id],
+				));
+			}
+
+		}
+
+		// If POST for deleting
+		if( isset( $_POST['type']) && $_POST['type'] == 'delete_' . $setting_name ) {
+			foreach ( $locales as $locale ) {
+				$GLOBALS['wpdb']->query( $GLOBALS['wpdb']->prepare(
+					"DELETE FROM $mixed_table_name WHERE $setting_name = %d", $_POST['setting_id']
+				));
+			}
+			$result = $GLOBALS['wpdb']->query( $GLOBALS['wpdb']->prepare(
+				"DELETE FROM " . $table_name . " WHERE id = %d", $_POST['setting_id']
+				));
+			if($result) {
+				?>
+			<div class="updated"><p><strong>Deleted.</strong></p></div>
+				<?php
+			} else {
+				?>
+			<div class="error"><p>Unable to delete.</p></div>
+			<?php
+			}
+		}
 	}
 
 	public static function wpri_form_add($form) {
@@ -87,7 +139,19 @@ class WPRI_Form {
 						elseif ($element["type"]=="radio"){?>
 							<div class='col-sm-3 form-element-caption'> <?php echo $element["caption"] ?></div>
 							<div class='col-sm-3 form-element-value'>   <?php echo $element["value"]   ?></div>
-						<?php }	?>
+						<?php }
+						elseif ($element["type"]=="multiple-select"){
+							$all_entries = WPRI_Database::get_all($form["source_table"]);
+							foreach ( $all_entries as $item ) {
+								echo "<div class='form-check'>";
+								echo "<label class='form-check-label'>";
+								echo "<input class='form-check-input' type='checkbox' name='ids[]' value='".$item->id."'>  ";
+								echo $item->$form["display_column"];
+								echo "</label>";
+								echo "</div>";
+							}
+						}
+						?>
 					<?php } ?>
 				<?php } ?>
 				<div class='col-sm-12 form-element-caption'>
@@ -97,7 +161,7 @@ class WPRI_Form {
 			</form>
 			</div>
 			<?php $all_entries = WPRI_Database::get_all($form["table_name"]);
-			echo "<h2>Existing</h2>";
+			echo "<h2>Already existing:</h2>";
 			echo "<ul class='list-group'>";
 			foreach ( $all_entries as $item ) {
 				echo "<li class='list-group-item'>".$item->name."</li>";
