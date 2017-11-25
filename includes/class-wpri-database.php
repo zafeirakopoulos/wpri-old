8c396f530b398d883ed0f84d4553e1ca65f1e9f8
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-26 02:04:11 +0300

    rewrite

--- includes/class-wpri-database.php
+++ includes/class-wpri-database.php
@@ -119,28 +119,28 @@ class WPRI_Database {
 
 	    $tables_to_drop = array();
 
-		foreach ($declarations as $entity_name => $entity) {
-			# Drop the table
-			$tables_to_drop.push($entity_name);
-
-			# Drop locale_* tables for all_locales elements
-			foreach ($entity["groups"] as $group ) {
-				foreach ($group["elements"] as $element ) {
-					if (isset($element["all_locales"]) ){
-						if ($entity_name!=$element["name"]){
-							$tables_to_drop.push("locale_".$entity_name."_".$element["name"]);
-						}
-						else{
-							$tables_to_drop.push("locale_".$element["name"]);
-						}
-					}
-				}
-			}
-
-		}
+		// foreach ($declarations as $entity_name => $entity) {
+		// 	# Drop the table
+		// 	$tables_to_drop.push($entity_name);
+        //
+		// 	# Drop locale_* tables for all_locales elements
+		// 	foreach ($entity["groups"] as $group ) {
+		// 		foreach ($group["elements"] as $element ) {
+		// 			if (isset($element["all_locales"]) ){
+		// 				if ($entity_name!=$element["name"]){
+		// 					$tables_to_drop.push("locale_".$entity_name."_".$element["name"]);
+		// 				}
+		// 				else{
+		// 					$tables_to_drop.push("locale_".$element["name"]);
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+        //
+		// }
 
 		array_reverse($tables_to_drop);
-		error_log("tables_to_drop".implode($tables_to_drop));
+		error_log("tables_to_drop ".implode($tables_to_drop));
 
 	    foreach($tables_to_drop as $table_name){
 		    $GLOBALS['wpdb']->query( "DROP TABLE IF EXISTS  " . self::table_name($table_name) );
