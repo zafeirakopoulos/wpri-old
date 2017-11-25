bd4ba60d8f40650e8a43a187c73e201b322785d3
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-25 15:07:38 +0300

    rewrite

--- includes/class-wpri-declarations.php
+++ includes/class-wpri-declarations.php
@@ -21,20 +21,18 @@
  * @author     Zafeirakis Zafeirakopoulos
  */
 class WPRI_Declarations {
-	$jsonfilename = "includes/declarations.json"
-	$jsonfile = fopen($jsonfilename, "r") or die("Unable to open file!");
-	$jsonstring =  fread($jsonfile,filesize($jsonfilename));
-	fclose($jsonfile);
-	
-	# Parse the json string as an associative array
-	$declarations = json_decode ($jsonstring , true )
-	echo $declarations;
 
-	function get($name) {
-		return $declarations[$name];
-	}
+
 
 	function get_declarations() {
+		$jsonfilename = "includes/declarations.json"
+		$jsonfile = fopen($jsonfilename, "r") or die("Unable to open file!");
+		$jsonstring =  fread($jsonfile,filesize($jsonfilename));
+		fclose($jsonfile);
+
+		# Parse the json string as an associative array
+		$declarations = json_decode ($jsonstring , true )
+		echo $declarations;
 		return $declarations;
 	}
 }
