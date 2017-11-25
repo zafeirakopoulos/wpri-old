872bc2f92a531a7102d0608ce02e3c27a2a2acf9
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-25 17:52:26 +0300

    rewrite

--- includes/class-wpri-database.php
+++ includes/class-wpri-database.php
@@ -54,7 +54,7 @@ class WPRI_Database {
 	public static function create_tables() {
 
 		$declarations = WPRI_Declarations::get_declarations();
-		
+		error_log( print_r($declarations, TRUE) );
  		foreach ($declarations as $entity_name => $entity) {
 			if( $GLOBALS['wpdb']->get_var( "SHOW TABLES LIKE '".self::table_name($entity_name)."'") != self::table_name($entity_name) ){
 				$sql = "CREATE TABLE ".self::table_name($entity_name)." ( id INT AUTO_INCREMENT PRIMARY KEY,";
