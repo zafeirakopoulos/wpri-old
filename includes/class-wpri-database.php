417ab5b4213b995c2102167354edcc640670f731
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-26 02:21:16 +0300

    rewrite

--- includes/class-wpri-database.php
+++ includes/class-wpri-database.php
@@ -141,6 +141,7 @@ class WPRI_Database {
 
 		$tables_to_drop = array_reverse($tables_to_drop);
 		error_log("tables_to_drop ".implode($tables_to_drop));
+		array_push($tables_to_drop,"locale_project_title");
 
 	    foreach($tables_to_drop as $table_name){
 		    $GLOBALS['wpdb']->query( "DROP TABLE IF EXISTS  " . self::table_name($table_name) );
