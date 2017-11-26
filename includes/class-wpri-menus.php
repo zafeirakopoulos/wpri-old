2044d3ccbbd53241a5e5cb047c6b6bc9ef5f63f4
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-27 00:06:31 +0300

    introduce menu class

--- includes/class-wpri-menus.php
+++ includes/class-wpri-menus.php
@@ -54,13 +54,11 @@ class WPRI_Menu {
 class wpri_menu_factory {
      public function __construct($entity) {
          $callback = '
-         function(){
              echo "<div class=\'wrap\'>";
              echo "<h2>"'.$entity["title"].'"</h2>";
              echo "</div>";
-         }
          ';
         error_log($callback);
-        add_menu_page( "wpri-".$entity["title"]."-menu" , $entity["title"], $entity["actions"]["add"], "wpri-".$entity["title"],create_function($callback));
+        add_menu_page( "wpri-".$entity["title"]."-menu" , $entity["title"], $entity["actions"]["add"], "wpri-".$entity["title"],create_function("$entity",$callback));
     }
 }
