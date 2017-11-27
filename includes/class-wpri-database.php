59a521748230e93e656073854cec91f45838cbe7
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-27 14:25:41 +0300

    introduce form class

--- includes/class-wpri-database.php
+++ includes/class-wpri-database.php
@@ -324,9 +324,9 @@ public static function add_localized($table,$item,$names) {
 
 		foreach ( WPRI_Database::get_locales() as $locale ) {
 		$GLOBALS['wpdb']->insert( self::table_name("locale_".$table) , array(
-			'locale' => $locale->id,
+			'locale' => $locale["id"],
 			$table => $new_id,
-			'name' => $names[$locale->id]
+			'name' => $names[$locale["id"]]
 		));
 		$success = $success * $GLOBALS['wpdb']->insert_id;
 	}
