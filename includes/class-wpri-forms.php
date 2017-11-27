f6c82f5eb68a70f06c946dcab5e6544c27be0ca1
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-27 21:19:29 +0300

    introduce from class

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -148,7 +148,7 @@ class WPRI_Form {
 							    Sortable.create(output,{sort:true, group:"<?php echo $element["name"]?>",
 									onUpdate: function(event, ui) {
 						                var order = this.toArray();
-						                jQuery('#<?php echo $element["name"]?>_order').val(order.join(","));
+						                document.getElementById('#<?php echo $element["name"]?>_order').val(order.join(","));
 						                alert($('#<?php echo $element["name"]?>_order').val());
 						            }
 								});
