44032228c5038e037aa3e30bafc2e5d0d30dc30a
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-11-27 20:33:06 +0300

    introduce from class

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -134,13 +134,9 @@ class WPRI_Form {
 							<li> other item </li>
 							<li> yet another item </li>
 							</ul>
-							<style>
-#sortable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
-#sortable li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; height: 18px; }
-#sortable li span { position: absolute; margin-left: -1.3em; }
-</style>
+
 							<script type="text/javascript">
-								$("contnet-box").sortable();
+								jQuery("contnet-box").sortable();
 
 							</script>
 							<?php
