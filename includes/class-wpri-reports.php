ae1880a2dd34184b192743c17dbdc1f35fd99925
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-05 21:27:23 +0300

    add post types

--- includes/class-wpri-reports.php
+++ includes/class-wpri-reports.php
@@ -76,7 +76,6 @@ class wpri_report_menu_factory {
         $callback = function() use ($entity){
             // WPRI_Form::wpri_create_form($entity);
         };
-		add_submenu_page( "wpri-reports-menu", "test","test",  "manage_options", "wpri-report-test",$callback);
     	add_submenu_page( "wpri-reports-menu", $entity["title"],$entity["title"], $entity["actions"]["add"], "wpri-report-".$entity["title"],$callback);
     }
 }
