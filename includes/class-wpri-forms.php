63200aa5d96ed8489fd929b9cc6b3625dc404f2f
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-05 14:33:40 +0300

    introduce from class

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -101,6 +101,7 @@ class WPRI_Form {
                             else{
                                 $relations[$relation["foreach"]["table"]] = array($tmp) ;
                             }
+                            error(print_r($relations[$relation["foreach"]["table"]]));
 						}
 					} elseif ($element["type"]== "select"){
 						$relations[$element["name"]] =  $_POST[$element["name"]] ;
