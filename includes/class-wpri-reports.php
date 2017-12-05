474eb8fb4352388a7cefca78a325949ea13528aa
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-05 22:41:12 +0300

    add post types

--- includes/class-wpri-reports.php
+++ includes/class-wpri-reports.php
@@ -74,7 +74,7 @@ class WPRI_Report {
 class wpri_report_menu_factory {
      public function __construct($entity) {
         $callback = function() use ($entity){
-			echo "<button class='navbar-btn' onclick='wpri_get_report(1)'> report</button>";
+			echo "<button class='navbar-btn' onclick='wpri_get_report()'> report</button>";
         };
     	add_submenu_page( "wpri-reports-menu", $entity["title"],$entity["title"], $entity["actions"]["add"], "wpri-report-".$entity["title"],$callback);
     }
