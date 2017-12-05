088096442095a966185dbe0478385170b8421b6e
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-06 00:26:14 +0300

    add post types

--- admin/js/wpri-admin.js
+++ admin/js/wpri-admin.js
@@ -5,10 +5,21 @@ function wpri_get_report(report_form) {
 			'action': 'get_report',
 			'data':  JSON.stringify({'report_form' : report_form})
 		},
-		function(response){
-		   // location.reload();
-		   alert('The server responded: '+ response);
-		}
+		// function(response){
+		//    // location.reload();
+		//    alert('The server responded: '+ response);
+		// }
+		function(response, status, request) {
+            var form = $jQuery('<form method="POST" action="' + url + '">');
+            // jQuery.each(params, function(k, v) {
+            //     form.append(jQuery('<input type="hidden" name="' + k +
+            //             '" value="' + v + '">'));
+            // });
+			form.append(jQuery('<input type="hidden" name="excelfile" value="' + data['report_form'] + '">'));
+	        jQuery('body').append(form);
+            form.submit();
+
+    	}
 	);
 
 
