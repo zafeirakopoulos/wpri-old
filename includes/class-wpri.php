d12d1fcee55c65b5b994e242dda8f95ce25c6f9f
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-05 23:50:38 +0300

    add post types

--- includes/class-wpri.php
+++ includes/class-wpri.php
@@ -236,11 +236,11 @@ class WPRI {
 		$this->loader->	add_action( 'admin_menu', $plugin_admin, 'settings_institute_info_menu' );
 
 
- 
+
 		$plugin_report = new WPRI_Report( $this->get_plugin_name(), $this->get_version() );
 		$this->loader->	add_action( 'admin_menu', $plugin_report, 'wpri_reports' );
 
-		$this->loader->	add_action( 'wp_ajax_wpri_get_report', $plugin_report, 'get_report' );
+		$this->loader->	add_action( 'wp_ajax_get_report', $plugin_report, 'get_report' );
 
 
 	}
