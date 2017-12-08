f40903fb70a8d740ef479091684183fa9f79d0ab
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-08 11:43:22 +0300

    add post types

--- includes/class-wpri-reports.php
+++ includes/class-wpri-reports.php
@@ -88,7 +88,7 @@ class wpri_report_menu_factory {
 			// <button class='navbar-btn' onclick='wpri_get_report(\"excel_report.xlsx\")'> report</button>
 			// </form>";
 
-			// echo '<button type="submit" onclick="window.open(\'excel_report.xlsx\')">Download!</button>';
+			 echo '<button type="submit" onclick="window.open(\'excel_report.xlsx\')">Download!</button>';
 			// echo "	<button class='navbar-btn' onclick='window.location.href = '".dirname( __FILE__ )."/report.php> report</button>";
 			};
     	add_submenu_page( "wpri-reports-menu", $entity["title"],$entity["title"], $entity["actions"]["add"], "wpri-report-".$entity["title"],$callback);
