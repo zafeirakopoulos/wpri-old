9913f79d623f42b59e700853dd313c87bb1b6aad
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-08 19:23:31 +0300

    add post types

--- includes/report.php
+++ includes/report.php
@@ -41,13 +41,13 @@ if ($_GET["id"]=="projects") {
         }
         $agency = join(",", $agency);
         $budget=$project["budget"];
-        $members = WPRI_Database::get_double_relation("project","member", "projectrole", $project["id"],"","");
-        $member= array();
-        foreach($members as $membr){
-            $mem = WPRI_Database::get_record("member",$membr["member"]);
-            array_push($member,["name"] );
-        }
-        $member = join(",", $member);
+        // $members = WPRI_Database::get_double_relation("project","member", "projectrole", $project["id"],"","");
+        // $member= array();
+        // foreach($members as $membr){
+        //     $mem = WPRI_Database::get_record("member",$membr["member"]);
+        //     array_push($member,["name"] );
+        // }
+        // $member = join(",", $member);
         $role=" ";
         $collaborators=" ";
 
