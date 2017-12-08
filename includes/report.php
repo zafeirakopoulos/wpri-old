077d751d541ca752555053ea39203809a9612352
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-08 18:05:09 +0300

    add post types

--- includes/report.php
+++ includes/report.php
@@ -13,16 +13,21 @@ header('Pragma: public');
 $writer = new XLSXWriter();
 $writer->setAuthor('Some Author');
 
-if ($_GET["id"]==1) {
+if ($_GET["id"]=="projects") {
+
+    $sheet1 = 'Projects';
+    $header = array("string","string","string","string","string");
+    $writer->writeSheetHeader($sheet1, $header, $col_options = ['suppress_row'=>true] );
 
     $rows = array(
         array('2003','1','-50.5','2010-01-01 23:00:00','2012-12-31 23:00:00'),
         array('2003','=B1', '23.5','2010-01-01 00:00:00','2012-12-31 00:00:00'),
     );
 
-    foreach($rows as $row)
-    	$writer->writeSheetRow('Sheet1', $row);
-}
+    foreach($rows as $row){
+        $writer->writeSheetRow($sheet1, $row);
+    }
+ }
 else {}
 
 $writer->writeToStdOut();
