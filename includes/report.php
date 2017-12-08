fa1deebe5fc2861c7a74a316ce200d8e7ad55c1c
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-08 18:09:25 +0300

    add post types

--- includes/report.php
+++ includes/report.php
@@ -16,12 +16,14 @@ $writer->setAuthor('Some Author');
 if ($_GET["id"]=="projects") {
 
     $sheet1 = 'Projects';
-    $header = array("string","string","string","string","string");
+    $header = array("string","string","string","string","string","integer");
     $writer->writeSheetHeader($sheet1, $header, $col_options = ['suppress_row'=>true] );
+    $header_format = array('font'=>'Arial','font-size'=>10,'font-style'=>'bold,italic', 'fill'=>'#eee','color'=>'#f00','fill'=>'#ffc', 'border'=>'top,bottom', 'halign'=>'center');
+    $writer->writeSheetRow($sheet1, array("Title","Member","Role","Status","Collaboratos","Budget"));
 
     $rows = array(
-        array('2003','1','-50.5','2010-01-01 23:00:00','2012-12-31 23:00:00'),
-        array('2003','=B1', '23.5','2010-01-01 00:00:00','2012-12-31 00:00:00'),
+        array('2003','1','-50.5','2010-01-01 23:00:00','2012-12-31 23:00:00',"233233"),
+        array('2003','=B1', '23.5','2010-01-01 00:00:00','2012-12-31 00:00:00',"2423423"),
     );
 
     foreach($rows as $row){
