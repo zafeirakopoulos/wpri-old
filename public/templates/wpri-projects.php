0193660390892c8d506d358d41a3f9ab8abb911b
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 15:35:55 +0300

    may break

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -4,7 +4,7 @@
 			$project_ids= WPRI_Database::get_ids("project");
  			foreach ( $project_ids as $project_id ) {
 				$project = WPRI_Database::get_entity("project",$project_id);
-					echo "<a class='listing-thumb' href='".site_url()."/project?id=".$project_id."'>";
+					echo "<a class='listing-thumb-frame' href='".site_url()."/project?id=".$project_id."'>";
 						echo "<div class='col-xs-3  listing-thumb'>"."picture"."</div>";
 						echo "<div class='col-xs-9 listing-thumb'><h1 class='listing'>".$project['title']."</h1> </div>";
 					echo "</a>";
