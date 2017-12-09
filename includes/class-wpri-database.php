cd2d998cc0bcf352b4c5145f1989096105935344
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 13:04:06 +0300

    add templates

--- includes/class-wpri-database.php
+++ includes/class-wpri-database.php
@@ -586,7 +586,7 @@ public static function add_form($entity, $form) {
 			 "SELECT * FROM " . self::table_name($entity_name). " WHERE id = %d", $id) ,"ARRAY_A"
 		 )[0];
 
-		 error_log(print_r("local:".print_r($local)));
+		 error_log("local:".print_r($local));
 
 	 	foreach (  $local  as $localizedname) {
 	 		$results[$localizedname] = WPRI_Database::get_localized_element($localizedname[0],$localizedname[1],$id);
