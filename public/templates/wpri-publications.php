5c41c2a5a6bf1d9b24490e6ccefe20f905a27ff1
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 17:22:38 +0300

    add templates

--- public/templates/wpri-publications.php
+++ public/templates/wpri-publications.php
@@ -8,7 +8,8 @@
 					echo "<a class='listing-thumb' href='".site_url()."/publication?id=".$publication_id."'>";
 						echo "<div class='col-xs-1  listing-thumb'>";
                         if (in_array($publication['pubtype'],array(1,2,4,6,7))) {
-                            echo "<img src='".plugin_dir_url( __FILE__ ).'..'."/clarivate.png' alt='SCI indexed' height='42' width='100'>";
+                            echo "<img src='".plugin_dir_url( __FILE__ ).".."."/clarivate.png' alt='SCI indexed' height='42' width='100'>";
+                            error_log(plugin_dir_url( __FILE__ ).".."."/clarivate.png");
                         }
                         echo "</div>";
 						echo "<div class='col-xs-11 listing-thumb'><h1 class='listing'>".$publication['title']."</h1> </div>";
