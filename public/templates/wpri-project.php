fe11f925d11def1a7c73564f3e66e0d31806dd8c
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 16:25:49 +0300

    may break

--- public/templates/wpri-project.php
+++ public/templates/wpri-project.php
@@ -3,7 +3,7 @@
 
 		<?php
 		$project_id=$_GET['id'];
-		$project = WPRI_Database::get_project_full($project_id);
+		$project = WPRI_Database::get_entity("project",$project_id);
 		?>
 		<div class='row'>
 			<div class='col-sm-12 col-md-12 col-lg-12 single'><h1 class=" single"> <?php echo $project['title'];?></h1> </div>
@@ -51,3 +51,25 @@
 	Needs work. From project management connect a wpri_news post with the project to query from here.
 	-->
 </div><!-- #project -->
+
+<!--
+echo "<div class='col-sm-12 listing-thumb-frame'>";
+	echo "<a class='listing-thumb' href='".site_url()."/project?id=".$project_id."'>";
+		echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>"."picture"."</div>";
+	echo "<div class='row'>";
+		echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$project['title']."</h1> </div>";
+	echo "</div>";
+	echo "</a>";
+
+	echo "<div class='row'>";
+		echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$project['status']."</div>";
+		if (isset($project['website']) AND $project['website']!=""){
+			echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$project['website']."</div>";
+		}
+		else{
+			echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>";
+			echo "<a class='listing-thumb' href='".site_url()."/project?id=".$project_id."'>".site_url()."/project?id=".$project_id."</a>";
+			echo "</div>";
+		}
+		echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>PI: ".$project['PI']."</div>";
+	echo "</div>"; -->
