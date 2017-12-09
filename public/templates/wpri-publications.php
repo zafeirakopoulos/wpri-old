c9032628fffa779f7d875517badd55d0a31c268a
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 15:23:11 +0300

    add templates

--- public/templates/wpri-publications.php
+++ public/templates/wpri-publications.php
@@ -7,9 +7,12 @@
 				$publication = WPRI_Database::get_entity("publication",$publication_id);
 				echo "<div class='col-sm-12 listing-thumb-frame'>";
 					echo "<a class='listing-thumb' href='".site_url()."/publication?id=".$publication_id."'>";
-						echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>"."picture"."</div>";
-					echo "<div class='row'>";
-						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$publication['title']."</h1> </div>";
+						echo "<div class='col-xs-1  listing-thumb'>";
+                        if () {
+                            echo "<img src='clarivate.png' alt='SCI indexed' height='42' width='100'>";
+                        }
+                        echo "</div>";
+						echo "<div class='col-xs-11 listing-thumb'><h1 class='listing'>".$publication['title']."</h1> </div>";
 					echo "</div>";
 					echo "</a>";
 				echo "</div>";
