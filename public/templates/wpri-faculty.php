bbca82a17af14a833d4671dd8cee8a531801f144
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 01:15:15 +0300

    add templates

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -24,7 +24,7 @@
     foreach ( $faculty as $member_id ) {
         $member = $members[$member_id];
 		echo "  <a href='".site_url()."/member?id=".$member["id"]."' class='list-group-item list-group-item-action'>";
-            echo "<div class='d-flex w-100 justify-content-between'> <h5>".$member["title"]." ".$member['name']."</h5><span>".get_avatar($member['user'])."</span></div>";
+            echo "<div> <h5>".$member["title"]." ".$member['name']."</h5><span>".get_avatar($member['user'])."</span></div>";
 			echo "</a>";
             //
 			// 	echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['position']."</div>";
