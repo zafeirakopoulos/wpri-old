b1258b71b5bf3f3aecd0af655488b5d66d058ae5
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 21:59:12 +0300

    add templates

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -1,32 +1,49 @@
 <div class='listing' id="faculty" >
- 		<div class='row'>
-			<?php
-			// Start the Loop.
-			$member_ids = WPRI_Database::get_member_ids();
-			foreach ( $member_ids as $member_id ) {
-				$member = WPRI_Database::get_member_short($member_id);
-				echo "<div class='col-sm-12 listing-thumb-frame'>";
-					echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member_id."'>";
-						echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>".get_avatar( $member['user'])."</div>";
-					echo "<div class='row'>";
-						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$member['title']." ".$member['name']."</h1> </div>";
-					echo "</div>";
-					echo "</a>";
-					echo "<div class='row'>";
-						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['position']."</div>";
-						if (isset($member['website']) AND $member['website']!=""){
-							echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['website']."</div>";
-						}
-						else{
-							echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>";
-							echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member_id."'>".site_url()."/member?id=".$member_id."</a>";
-							echo "</div>";
-						}
-						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['email']."</div>";
-					echo "</div>";
+	<?php
+	$members= WPRI_Database::get_all("member");
+    $faculty = array();
+    $assistants = array();
+    $administrative = array();
+    foreach ( $members as $member ) {
+        if (in_array($member["position"],array(1,2,3))){
+            $faculty[] = $member;
+        }
+        if (in_array($member["position"],array(4,5))){
+            $assistants[] = $member;
+        }
+        if (in_array($member["position"],array(6))){
+            $administrative[] = $member;
+        }
+    }
 
-				echo "</div>";
-			}
- 			?>
-		</div>
+    echo "<h1>Faculty</h1>";
+	foreach ( $faculty as $member ) {
+		echo "<div class='col-sm-12 listing-thumb-frame'>";
+			echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member["id"]."'>";
+				echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>".get_avatar( $member['user'])."</div>";
+			echo "<div class='row'>";
+				echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$member['title']." ".$member['name']."</h1> </div>";
+			echo "</div>";
+			echo "</a>";
+
+				echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['position']."</div>";
+				if (isset($member['website']) AND $member['website']!=""){
+					echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['website']."</div>";
+				}
+				else{
+					echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>";
+					echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member_id."'>".site_url()."/member?id=".$member_id."</a>";
+					echo "</div>";
+				}
+				echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['email']."</div>";
+			echo "</div>";
+
+		echo "</div>";
+	}
+
+    echo "<h1>Assistants</h1>";
+
+    echo "<h1>Administrative personel</h1>";
+
+		?>
  </div><!-- #faculty -->
