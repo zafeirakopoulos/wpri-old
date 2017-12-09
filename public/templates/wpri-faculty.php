5b757973b0636132871dbd7c4de9b042711bb733
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 22:06:03 +0300

    add templates

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -42,7 +42,29 @@
 	}
 
     echo "<h1>Assistants</h1>";
+    foreach ( $assistants as $member ) {
+		echo "<div class='col-sm-12 listing-thumb-frame'>";
+			echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member["id"]."'>";
+				echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>".get_avatar( $member['user'])."</div>";
+			echo "<div class='row'>";
+				echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$member['title']." ".$member['name']."</h1> </div>";
+			echo "</div>";
+			echo "</a>";
 
+				echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['position']."</div>";
+				if (isset($member['website']) AND $member['website']!=""){
+					echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['website']."</div>";
+				}
+				else{
+					echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>";
+					echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member_id."'>".site_url()."/member?id=".$member_id."</a>";
+					echo "</div>";
+				}
+				echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['email']."</div>";
+			echo "</div>";
+
+		echo "</div>";
+	}
     echo "<h1>Administrative personel</h1>";
 
 		?>
