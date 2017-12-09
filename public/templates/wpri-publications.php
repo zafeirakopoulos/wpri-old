7ff04968239223c5fe83403ffb47068437aa9066
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 15:17:26 +0300

    add templates

--- public/templates/wpri-publications.php
+++ public/templates/wpri-publications.php
@@ -1,15 +1,15 @@
-<div class='listing' id="faculty" >
+<div class='listing' id="publications" >
  		<div class='row'>
 			<?php
 			// Start the Loop.
-			$publication_ids = WPRI_Database::get_publication_ids();
+			$publication_ids = WPRI_Database::get_ids("publication");
 			foreach ( $publication_ids as $publication_id ) {
-				$publication = WPRI_Database::get_publication_short($publication_id);
+				$publication = WPRI_Database::get_entity("publication",$publication_id);
 				echo "<div class='col-sm-12 listing-thumb-frame'>";
 					echo "<a class='listing-thumb' href='".site_url()."/publication?id=".$publication_id."'>";
 						echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>"."picture"."</div>";
 					echo "<div class='row'>";
-						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$publication['title']." ".$member['name']."</h1> </div>";
+						echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$publication['title']."</h1> </div>";
 					echo "</div>";
 					echo "</a>";
 				echo "</div>";
