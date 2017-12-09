5f2bbf83d852f12aa8dc5bd1f6b0359f2239d032
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 20:43:56 +0300

    add templates

--- public/templates/wpri-publication.php
+++ public/templates/wpri-publication.php
@@ -3,25 +3,16 @@
 	<div class="container">
 		<?php
 		$publication_id=$_GET['id'];
-		$publication = WPRI_Database::get_publication_full($publication_id);
+		$publication = WPRI_Database::get_entity("publication",$publication_id);
 		?>
-		<div class='row'>
-			<div class='col-sm-12 col-md-12 col-lg-12 single'><h1 class=" single"> <?php echo $publication['title'];?></h1> </div>
-		</div>
-		<div class='row'>
-			<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo "publication picture"?> </div>
-			<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $publication['doi'];?> </div>
-			<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $publication["pubtype"];?> </div>
-		</div>
-		<div class='row'>
-			<div class='col-sm-12 col-md-12 col-lg-12 wordwrap  single'> <?php echo $publication['bibentry'];?> </div>
-		</div>
+		<div class='col-sm-12 col-md-12 col-lg-12 single'><h1 class=" single"> <?php echo $publication['title'];?></h1> </div>
+		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo "publication picture"?> </div>
+		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $publication['doi'];?> </div>
+		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $publication["pubtype"];?> </div>
+		<div class='col-sm-12 col-md-12 col-lg-12 wordwrap single'> <?php echo $publication['bibentry'];?> </div>
 	</div>
 
-
-
-	<h2 class=" single">Authors:</h2>
-	<div class="container">
+	<h2 class=" single">Authors</h2>
 	<?php
 		foreach ($publication['authors'] as $author) {
 			$member = WPRI_Database::get_member($author->member);
@@ -31,7 +22,10 @@
 			echo "</div>";
 			echo "</a>";
 		}?>
-	</div>
+
+
+
+
 
 	<h2 class=" single">Projects:</h2>
 	<div class="container">
