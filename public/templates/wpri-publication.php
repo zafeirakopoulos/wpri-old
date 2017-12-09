a5447b952850a27452dabde56a25f5b045325d18
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-09 21:09:47 +0300

    add templates

--- public/templates/wpri-publication.php
+++ public/templates/wpri-publication.php
@@ -7,34 +7,35 @@
 		?>
 		<div class='col-sm-12 col-md-12 col-lg-12 single'><h1 class=" single"> <?php echo $publication['title'];?></h1> </div>
 		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo "publication picture"?> </div>
+		<div class='col-sm-12 single'> <?php echo $publication['authors'];?> </div>
 		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $publication['doi'];?> </div>
 		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $publication["pubtype"];?> </div>
-		<div class='col-sm-12 col-md-12 col-lg-12 wordwrap single'> <?php echo $publication['bibentry'];?> </div>
+		<div class='col-sm-12 col-md-12 col-lg-12 wordwrap single'> <?php echo $publication['bibtex'];?> </div>
 	</div>
 
 	<h2 class=" single">Authors</h2>
 	<?php
-		foreach ($publication['authors'] as $author) {
-			$member = WPRI_Database::get_member($author->member);
-			echo "<a class=' single' href='".site_url()."/member?id=".$author->member."'>";
+		foreach ($publication['member'] as $member_id) {
+			$member = WPRI_Database::get_record("member",$member_id) ;
+			echo "<a class=' single' href='".site_url()."/member?id=".$member_id."'>";
 			echo "<div class='row'>";
-				echo "<div class='col-sm-12 col-md-12 col-lg-12 single'>".$member['name']."</div>";
+				echo "<div class='col-sm-12 single'>".$member["name"]."</div>";
 			echo "</div>";
 			echo "</a>";
 		}?>
 
 
-
-
-
 	<h2 class=" single">Projects:</h2>
 	<div class="container">
 		<?php
-		foreach ($publication['projects'] as $project_row) {
-			$project = WPRI_Database::get_project($project_row->project);
-			echo "<a class=' single' href='".site_url()."/project?id=".$project_row->project."'>";
+		foreach ($publication['project'] as $project_id) {
+			$project = WPRI_Database::get_record("project",$project_id) ;
+			echo "<a class=' single' href='".site_url()."/project?id=".$project_id."'>";
 			echo "<div class='row'>";
-				echo "<div class='col-sm-12 col-md-12 col-lg-12 single'>".$project->title."</div>";
+				echo "<div class='col-sm-12 col-md-12 col-lg-12 single'>".
+				$project[title
+				WPRI_Database::get_localized_element("project","title",$project_id)
+				."</div>";
 			echo "</div>";
 			echo "</a>";
 		}?>
