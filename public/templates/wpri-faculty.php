d5d22d5ef5f4ca27cc1cdbd642caf94be369a845
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 02:26:07 +0300

    add templates

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -24,7 +24,7 @@
     foreach ( $faculty as $member_id ) {
         $member = $members[$member_id];
 		echo "<a href='".site_url()."/member?id=".$member["id"]."' class='single'>
-                <div class='col-xs-6 single'>
+                <div class='col-xs-12 single'>
                     <div class='col-xs-12 single'><h1 class='single'>".$member["title"]." ".$member['name']."</h1></div>
                     <div class='col-xs-3 single'>".get_avatar($member['user'])."</div>
                     <div class='col-xs-9 single'>".$member['position']."</div>
@@ -34,29 +34,24 @@
      echo "<h1>Assistants</h1>";
     foreach ( $assistants as $member_id ) {
         $member = $members[$member_id];
-		echo "<div class='col-sm-12 listing-thumb-frame'>";
-			echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member["id"]."'>";
-				echo "<div class='col-xs-12 col-md-6 col-ld-12  listing-thumb'>".get_avatar($member['user'])."</div>";
-			// echo "<div class='row'>";
-			echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'><h1 class='listing'>".$member["title"]." ".$member['name']."</h1> </div>";
-			echo "</div>";
-			echo "</a>";
-            //
-			// 	echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['position']."</div>";
-			// 	if (isset($member['website']) AND $member['website']!=""){
-			// 		echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['website']."</div>";
-			// 	}
-			// 	else{
-			// 		echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>";
-			// 		echo "<a class='listing-thumb' href='".site_url()."/member?id=".$member_id."'>".site_url()."/member?id=".$member_id."</a>";
-			// 		echo "</div>";
-			// 	}
-			// 	echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['email']."</div>";
-			echo "</div>";
-
-		echo "</div>";
+        echo "<a href='".site_url()."/member?id=".$member["id"]."' class='single'>
+                <div class='col-xs-12 single'>
+                    <div class='col-xs-12 single'><h1 class='single'>".$member["title"]." ".$member['name']."</h1></div>
+                    <div class='col-xs-3 single'>".get_avatar($member['user'])."</div>
+                    <div class='col-xs-9 single'>".$member['position']."</div>
+                </div>
+            </a>";
 	}
     echo "<h1>Administrative personel</h1>";
-
+    foreach ( $administrative as $member_id ) {
+        $member = $members[$member_id];
+        echo "<a href='".site_url()."/member?id=".$member["id"]."' class='single'>
+                <div class='col-xs-12 single'>
+                    <div class='col-xs-12 single'><h1 class='single'>".$member["title"]." ".$member['name']."</h1></div>
+                    <div class='col-xs-3 single'>".get_avatar($member['user'])."</div>
+                    <div class='col-xs-9 single'>".$member['position']."</div>
+                </div>
+            </a>";
+	}
 		?>
  </div><!-- #faculty -->
