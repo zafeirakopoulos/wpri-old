86515f1a4b4f11fc0aab2136dff0ddf8162c39ad
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 01:39:41 +0300

    add templates

--- public/templates/wpri-faculty.php
+++ public/templates/wpri-faculty.php
@@ -43,7 +43,15 @@
 			// 		echo "</div>";
 			// 	}
 			// 	echo "<div class='col-xs-12 col-md-6 col-ld-12 listing-thumb'>".$member['email']."</div>";
-	}
+	}?>
+    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+  <div class="d-flex w-100 justify-content-between">
+    <h5 class="mb-1">List group item heading</h5>
+    <small>3 days ago</small>
+  </div>
+  <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+  <small>Donec id elit non mi porta.</small>
+</a> <?php
     echo "</div>";
     echo "<h1>Assistants</h1>";
     foreach ( $assistants as $member_id ) {
