5cbd402f556f2d86216835e3d72c4dc7e311fb58
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 23:28:31 +0300

    add templates

--- includes/class-wpri-reports.php
+++ includes/class-wpri-reports.php
@@ -26,12 +26,15 @@ class WPRI_Report {
 	public static function wpri_reports() {
 		$callback = function() {
 			?>
+			<br>
+			<h1>Reports</h1>
+			<br>
 			<div class='row'>
 			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = \"../report?id=projects\"'> Projects</button></div>
 			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = \"../report?id=2\"'> report 2</button></div>
 			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = \"../report?id=3\"'> report3</button></div>
 			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = \"../report?id=3\"'> report7</button></div>
-		</div><br>
+			</div><br>
 			<div class='row'>
 			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = \"../report?id=2\"'> report4</button></div>
 			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = \"../report?id=3\"'> report5</button></div>
