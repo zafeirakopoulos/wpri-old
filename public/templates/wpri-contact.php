ff3d260588e3e19ea90bae558facca291ce63090
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 17:28:12 +0300

    add templates

--- public/templates/wpri-contact.php
+++ public/templates/wpri-contact.php
@@ -3,15 +3,15 @@
 		<div class='col-sm-12 col-md-12 col-lg-12 single'>
 			<h2 class=" single">Address:</h2>
 			<div class='row'>
-				<div class='col-sm-12 col-md-12 col-lg-12 single'>Gebze Technical University Computer Engineering Building </div>
-				<div class='col-sm-12 col-md-12 col-lg-12 single'>Gebze – Kocaeli – Turkey </div>
-				<div class='col-sm-12 col-md-12 col-lg-12 single'>ZIP Code : 41400 </div>
+				<div class='col-sm-12 col-md-12 col-lg-12 single'><h3 class="single">Gebze Technical University Computer Engineering Building</h3> </div>
+				<div class='col-sm-12 col-md-12 col-lg-12 single'><h3 class="single">Gebze – Kocaeli – Turkey </h3></div>
+				<div class='col-sm-12 col-md-12 col-lg-12 single'><h3 class="single">ZIP Code : 41400 </h3></div>
 			</div>
 	 		<div class='row'>
-				<div class='col-sm-3 col-md-3 col-lg-3 single'>Phone:</div>
-				<div class='col-sm-9 col-md-9 col-lg-9 single'>+90 (262) 605 2203</div>
-				<div class='col-sm-3 col-md-3 col-lg-3 single'>E-mail</div>
-				<div class='col-sm-9 col-md-9 col-lg-9 single'>bte {at} gtu.edu.tr</div>
+				<div class='col-sm-3 col-md-3 col-lg-3 single'><h3 class="single">Phone:</h3></div>
+				<div class='col-sm-9 col-md-9 col-lg-9 single'><h3 class="single">+90 (262) 605 2203</h3></div>
+				<div class='col-sm-3 col-md-3 col-lg-3 single'><h3 class="single">E-mail</h3></div>
+				<div class='col-sm-9 col-md-9 col-lg-9 single'><h3 class="single">bte {at} gtu.edu.tr</h3></div>
 			</div>
 		</div>
 	</div>
--- public/templates/wpri-research.php
+++ public/templates/wpri-research.php
@@ -2,12 +2,12 @@
 	<div class="row">
 		<div class='col-sm-12 col-md-12 col-lg-12 single'>
 			<h1>Research</h1>
-			<ul>
-				<li> Cybersecurity – Digital Forensics</li>
-				<li> Big Data – Cloud Computing</li>
-				<li> Autonomous Systems – Vehicles</li>
-				<li> Information Networks</li>
-				<li> Bioinformatics – Cognitive Sciences</li>
+			<ul class="list-group">
+				<li class="list-grou-item"> Cybersecurity – Digital Forensics</li>
+				<li class="list-grou-item"> Big Data – Cloud Computing</li>
+				<li class="list-grou-item"> Autonomous Systems – Vehicles</li>
+				<li class="list-grou-item"> Information Networks</li>
+				<li class="list-grou-item"> Bioinformatics – Cognitive Sciences</li>
 			</ul>
 		</div>
 	</div>
