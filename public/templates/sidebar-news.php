853f74a97a2d47dffe3130ed2aa993e4a8da952a
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 14:22:36 +0300

    add templates

--- public/templates/sidebar-news.php
+++ public/templates/sidebar-news.php
@@ -1,18 +1,14 @@
-<div class="wpri-sidebar-frame" >
+<div>
 	<h1 class='wpri-sidebar'> News </h1>
-	<div class="wpri-sidebar">
-		<ol class="wpri-sidebar">
+		<ul class="list-group">
 			<?php
 				$args = array( 'post_type' => 'wpri_news', 'posts_per_page' => 10 );
 				$loop = new WP_Query( $args );
 				while ( $loop->have_posts() ) : $loop->the_post();
-					echo "<a class='faculty-thumb-link' href='".site_url()."/news/".$post->post_name."'>";
-						echo '<div>';
+					echo "<a class='list-group-item' href='".site_url()."/news/".$post->post_name."'>";
 						the_title();
-		  				echo '</div>';
 					echo "</a>";
 				endwhile;
 			?>
-		</ol>
-	</div>
+		</ul>
 </div>
