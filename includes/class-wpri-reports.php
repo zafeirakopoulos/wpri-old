7c32b728c173115459a84dcd1ea42522c0ca4f5e
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 23:34:33 +0300

    add templates

--- includes/class-wpri-reports.php
+++ includes/class-wpri-reports.php
@@ -30,16 +30,16 @@ class WPRI_Report {
 			<h1>Reports</h1>
 			<br>
 			<div class='row'>
-			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=projects"'> Projects</button></div>
-			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=2"'> report 2</button></div>
-			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=3"'> report3</button></div>
-			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=3"'> report7</button></div>
+			<div class='col-xs-12 col-md-6 col-lg-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=projects"'> Projects</button></div>
+			<div class='col-xs-12 col-md-6 col-lg-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=2"'> report 2</button></div>
+			<div class='col-xs-12 col-md-6 col-lg-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=3"'> report3</button></div>
+			<div class='col-xs-12 col-md-6 col-lg-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=3"'> report7</button></div>
 			</div><br>
 			<div class='row'>
-			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=2"'> report4</button></div>
-			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=3"'> report5</button></div>
-			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=2"'> report6</button></div>
-			<div class='col-xs-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=3"'> report7</button></div>
+			<div class='col-xs-12 col-md-6 col-lg-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=2"'> report4</button></div>
+			<div class='col-xs-12 col-md-6 col-lg-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=3"'> report5</button></div>
+			<div class='col-xs-12 col-md-6 col-lg-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=2"'> report6</button></div>
+			<div class='col-xs-12 col-md-6 col-lg-3'><button class='btn btn-primary' style='width:50%;' onclick='window.location.href = "../report?id=3"'> report7</button></div>
 			</div>
 			<?php };
 		add_menu_page( "Reports Management", "Reports", "manage_options", "wpri-reports-menu",$callback);
