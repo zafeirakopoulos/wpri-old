914afc6ffd13619c81d61988926204ac2770cb51
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 12:02:47 +0300

    add templates

---
+++ public/templates/wpri-research.php
@@ -0,0 +1,14 @@
+<div class=" single">
+	<div class="row">
+		<div class='col-sm-12 col-md-12 col-lg-12 single'>
+			<h1>Research</h1>
+			<ul>
+				<li> Cybersecurity – Digital Forensics</li>
+				<li> Big Data – Cloud Computing</li>
+				<li> Autonomous Systems – Vehicles</li>
+				<li> Information Networks</li>
+				<li> Bioinformatics – Cognitive Sciences</li>
+			</ul>
+		</div>
+	</div>
+</div><!-- #contact -->
