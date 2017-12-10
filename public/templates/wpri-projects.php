a0976f2f6a2db621c00be4729a05a10eb93e9fdd
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 22:23:00 +0300

    add templates

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -1,4 +1,6 @@
 <div class='listing' id="projects" >
+    <h1 class="single"> Projects </h1>
+
  			<?php
 			$project_ids= WPRI_Database::get_ids("project");
  			foreach ( $project_ids as $project_id ) {
@@ -7,7 +9,7 @@
                 echo "<div class='row'>
                         <a href='".site_url()."/project?id=".$project_id."' class='single'>
                             <div class='col-xs-8 offset-xs-2 single'>
-                                <div class='col-xs-12 single'><h1 class='single'>".$project['title']."</h1></div>
+                                <div class='col-xs-12 single'><h2 class='single'>".$project['title']."</h2></div>
                                 <div class='col-xs-3 single'>".$project["picture"]."</div>
                                 <div class='col-xs-9 single'><h3 class='single'>Funded by: ".join(",",$project['agency'])."</h3></div>
                             </div>
