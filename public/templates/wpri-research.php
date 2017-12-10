3d548171d9fa4b916402770d221cfe1c56645cc2
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 17:29:19 +0300

    add templates

--- public/templates/wpri-research.php
+++ public/templates/wpri-research.php
@@ -3,11 +3,11 @@
 		<div class='col-sm-12 col-md-12 col-lg-12 single'>
 			<h1>Research</h1>
 			<ul class="list-group">
-				<li class="list-grou-item"> Cybersecurity – Digital Forensics</li>
-				<li class="list-grou-item"> Big Data – Cloud Computing</li>
-				<li class="list-grou-item"> Autonomous Systems – Vehicles</li>
-				<li class="list-grou-item"> Information Networks</li>
-				<li class="list-grou-item"> Bioinformatics – Cognitive Sciences</li>
+				<li class="list-group-item"> Cybersecurity – Digital Forensics</li>
+				<li class="list-group-item"> Big Data – Cloud Computing</li>
+				<li class="list-group-item"> Autonomous Systems – Vehicles</li>
+				<li class="list-group-item"> Information Networks</li>
+				<li class="list-group-item"> Bioinformatics – Cognitive Sciences</li>
 			</ul>
 		</div>
 	</div>
