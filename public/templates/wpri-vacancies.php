20978019e2ed57a316dc1971c44fa8def1911f72
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-11 01:18:28 +0300

    add templates

---
+++ public/templates/wpri-vacancies.php
@@ -0,0 +1,12 @@
+<div class='single' id="vanacies" >
+    <h1 class="single"> Open positions </h1>
+        <ul class="list-group">
+ 			<?php
+			$vacancy_ids= WPRI_Database::get_ids("vacancy");
+ 			foreach ( $vacancy_ids as $vacancy_id ) {
+				$vacancy = WPRI_Database::get_entity("vacancy",$vacancy_id);
+                echo "<a href='".site_url()."/project?id=".$vacancy_id."' class='list-group-item'>".$vacancy['official_title']." (".$vacancy['deadline'].")</a>";
+			}
+ 			?>
+        </ul>
+ </div>
