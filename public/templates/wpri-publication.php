5d7cd0914bdd7874f5811a27cc65c6276f8419d9
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 17:16:23 +0300

    add templates

--- public/templates/wpri-publication.php
+++ public/templates/wpri-publication.php
@@ -6,37 +6,28 @@
 		?>
 		<div class='col-sm-12 col-md-12 col-lg-12 single'><h1 class=" single"> <?php echo $publication['title'];?></h1> </div>
 		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo "publication picture"?> </div>
-		<div class='col-sm-12 single'> <?php echo $publication['authors'];?> </div>
-		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $publication['doi'];?> </div>
-		<div class='col-sm-3 col-md-3 col-lg-3 single'> <?php echo $publication["pubtype"];?> </div>
-		<div class='col-sm-12 col-md-12 col-lg-12 wordwrap single'> <?php echo $publication['bibtex'];?> </div>
+		<div class='col-sm-12 single'><h3 class=" single"> <?php echo $publication['authors'];?> </h3></div>
+		<div class='col-sm-3 col-md-3 col-lg-3 single'><h3 class=" single"> <?php echo $publication['doi'];?>  </h3></div>
+		<div class='col-sm-3 col-md-3 col-lg-3 single'><h3 class=" single"> <?php echo $publication["pubtype"];?> </h3> </div>
+		<div class='col-sm-12 col-md-12 col-lg-12 wordwrap single'><h3 class=" single"> <?php echo $publication['bibtex'];?>  </h3></div>
 
 	<h2 class=" single">Authors</h2>
+    <ul class="list-group">
 	<?php
 		foreach ($publication['member'] as $member_id) {
 			$member = WPRI_Database::get_record("member",$member_id) ;
-			echo "<a class=' single' href='".site_url()."/member?id=".$member_id."'>";
-			echo "<div class='row'>";
-				echo "<div class='col-sm-12 single'>".$member["name"]."</div>";
-			echo "</div>";
-			echo "</a>";
+			echo "<a class='list-group-item' href='".site_url()."/member?id=".$member_id."'>".$member["name"]."</a>";
 		}?>
-
+    </ul>
 
 	<h2 class=" single">Projects</h2>
-	<div class="container">
+    <ul class="list-group">
 		<?php
 		foreach ($publication['project'] as $project_id) {
 			$project = WPRI_Database::get_record("project",$project_id) ;
-			echo "<a class=' single' href='".site_url()."/project?id=".$project_id."'>";
-			echo "<div class='row'>";
-				echo "<div class='col-sm-12 col-md-12 col-lg-12 single'>".
-				WPRI_Database::get_localized_element("project","title",$project_id)
-				."</div>";
-			echo "</div>";
-			echo "</a>";
+			echo "<a class='list-group-item' href='".site_url()."/project?id=".$project_id."'>".WPRI_Database::get_localized_element("project","title",$project_id)."</a>";
 		}?>
-	</div>
+    </ul>
 
 
 </div><!-- #publication -->
