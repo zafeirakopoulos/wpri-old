66c92f529cda4d4abf4bb4491dbe5f1e10239fe4
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 23:12:06 +0300

    add templates

--- public/templates/page.php
+++ public/templates/page.php
@@ -39,7 +39,7 @@ $template_loader->get_template_part( 'wpri', 'header' );
                 $template_loader->get_template_part( 'wpri', 'publication' );
             }
 
-            if ( is_single() && !is_page( 'report' )) {
+            if ( is_single() ) {
                 $template_loader->get_template_part( 'wpri', 'single' );
             }
                 if ( have_posts() ) : while ( have_posts() ) : the_post();
