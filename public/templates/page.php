a335d42b6aa887bd183bb5e81069b213e7830d24
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-10 11:56:55 +0300

    add templates

--- public/templates/page.php
+++ public/templates/page.php
@@ -48,7 +48,7 @@ $template_loader->get_template_part( 'wpri', 'header' );
             */
             ?>
         </div> <!-- /.col -->
-        <div class="vr"></div>
+        <hr class="vr">
         <div class="col-xs-12 col-md-3">
             <?php
                 $template_loader->get_template_part( 'sidebar', 'news' );
