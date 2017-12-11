34fe3ee67b59807e70523ecc1a02f8d3f66f4a72
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-11 14:54:23 +0300

    add templates

--- public/templates/page.php
+++ public/templates/page.php
@@ -3,8 +3,7 @@ $template_loader = new WPRI_Template_Loader;
 $template_loader->get_template_part( 'wpri', 'header' );
 ?>
 <!-- <div class="container" style="background-image: url('http://bte.gtu.edu.tr/wp-content/uploads/2016/03/bg2.jpg'); "> -->
-<div class="main-content-container">
-<div class="container main-content-container">
+ <div class="container main-content-container">
     <div class="row">
         <div class="col-xs-12 col-md-9">
             <?php
@@ -60,7 +59,6 @@ $template_loader->get_template_part( 'wpri', 'header' );
         </div> <!-- /.col -->
 
     </div> <!-- /.row -->
-</div> <!-- /.container -->
 </div> <!-- /.container -->
 
 <?php
