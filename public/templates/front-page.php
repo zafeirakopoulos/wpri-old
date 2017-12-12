309ec3d5b4fac4e9d8e9ce753ef3d63ec231d630
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-12 11:23:32 +0300

    add templates

--- public/templates/front-page.php
+++ public/templates/front-page.php
@@ -9,7 +9,7 @@ $template_loader->get_template_part( 'wpri', 'header' );
         <div class="col-xs-12 col-md-9">
             <ul class="list-group">
                 <li class="list-group-item">
-     			  <h3> Gebze Technical University Institute of Information Technologies (GTU IIT) conducts qualified research and development activities at national and international level with an industrial perspective.
+     			  <h3 class="list-item"> Gebze Technical University Institute of Information Technologies (GTU IIT) conducts qualified research and development activities at national and international level with an industrial perspective.
     			   <br>
     			   The institute aims to be in cooperation with other departments, especially the GTU computer engineering department, domestic and foreign universities, public institutions, research centers and IT companies. IIT brings together researchers and resources from different disciplines aiming at collaboration.
     			   <br>
