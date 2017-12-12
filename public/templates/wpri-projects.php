46e7e5939ee38dc11d4fe5e572bf21f02ef19cca
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-12 10:09:55 +0300

    add templates

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -6,6 +6,18 @@
 			$project_ids= WPRI_Database::get_ids("project");
  			foreach ( $project_ids as $project_id ) {
 				$project = WPRI_Database::get_entity("project",$project_id);
+
+                echo "<div class='row'>
+                        <a href='".site_url()."/project?id=".$project_id."' class='single'>
+                            <div class='col-xs-8 offset-xs-2 single'>
+                                <div class='col-xs-12 single'><h2 class='single'>".$project['title']."</h2></div>
+                                <div class='col-xs-3 single'>".$project["picture"]."</div>
+                                <div class='col-xs-9 single'><h3 class='single'>Funded by: ".join(",",$project['agency'])."</h3></div>
+                            </div>
+                        </a>
+                        </div>
+                    <hr/>";
+
                     echo "<a class='list-group-item' href='".site_url()."/project?id=".$project_id."'>";
                         echo"<div'>
                             <div><h2 class='single'>".$project['title']."</h2></div>
