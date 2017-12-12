fc29bca729743a3d592497e7a2418014794581df
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-12 10:08:05 +0300

    add templates

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -1,5 +1,6 @@
 <div class='single' id="projects" >
     <h1 class="single"> Projects </h1>
+        <ul class="list-group">
 
  			<?php
 			$project_ids= WPRI_Database::get_ids("project");
@@ -16,6 +17,15 @@
                         </a>
                         </div>
                     <hr/>";
+
+                    echo "<a class='list-group-item' href='".site_url()."/project?id=".$project_id."'>";
+                        echo"<div'>
+                            <div><h2 class='single'>".$project['title']."</h2></div>
+                            <div>".$project["picture"]."</div>
+                            <div><h3 class='single'>Funded by: ".join(",",$project['agency'])."</h3></div>
+                        </div>"" ;
+                    echo "</a>";
 			}
  			?>
+        </ul>
   </div><!-- #projects -->
