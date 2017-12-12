5ce96b71c89b1a7a3b638d212c10b4b6f6e246a5
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-12 11:32:51 +0300

    add templates

--- public/templates/wpri-member.php
+++ public/templates/wpri-member.php
@@ -6,8 +6,8 @@
 		?>
 		<div class='row'>
 	        <a href='<?php echo site_url()."/member?id=".$member["id"];?>' class=''>
-                <div class='col-xs-8 offset-xs-2 '>
-                    <div class='col-xs-12 '><h1><?php echo $member["title"]." ".$member['name'];?></h1></div>
+                <div class='col-xs-8 offset-xs-2 list-group-item '>
+                    <div class='col-xs-12'><h1><?php echo $member["title"]." ".$member['name'];?></h1></div>
                     <div class='col-xs-3 '><?php echo get_avatar($member['user']);?></div>
                     <div class='col-xs-9 '><h3 class="single"><?php echo $member['position'];?></h3></div>
                 </div>
@@ -17,15 +17,15 @@
 
 	<h1 class="outfont"> Contact </h1>
 	<div class='row'>
-		<div class='col-xs-12 col-sm-4 '>
+		<div class='col-xs-12 col-sm-4 list-group-item'>
 				<h3 class="single">Office</h3> <br>
 				<h3 class="single"><?php echo $member['office'];?></h3>
 		</div>
-		<div class='col-xs-12 col-sm-4 '>
+		<div class='col-xs-12 col-sm-4 list-group-item'>
 				<h3 class="single">Phone</h3> <br>
 				<h3 class="single"><?php echo $member['phone'];?></h3>
 		</div>
-		<div class='col-xs-12 col-sm-4 '>
+		<div class='col-xs-12 col-sm-4 list-group-item'>
 				<h3 class="single">email</h3> <br>
 				<h3 class="single"><?php echo $member['email'];?></h3>
 			</p>
