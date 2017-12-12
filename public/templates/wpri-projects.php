e8ccbe1a875d4900231927fc6ebd130335b2989a
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-12 10:13:15 +0300

    add templates

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -19,11 +19,10 @@
                 //     <hr/>";
 
                     echo "<a class='list-group-item' href='".site_url()."/project?id=".$project_id."'>";
-                        echo"<div'>";
-                        echo "<div><h2 class='single'>".$project['title']."</h2></div>";
-                        echo "<div><h3 class='single'>Funded by: ".join(",",$project['agency'])."</h3></div>";
+                        // echo"<div'>";
+                        echo "<h2 class='single'>".$project['title']."</h2><br>";
+                        echo "<h3 class='single'>Funded by: ".join(",",$project['agency'])."</h3>";
                         // echo "<div>".$project["picture"]."</div>";
-                        echo "</div>" ;
                     echo "</a>";
 			}
  			?>
