b4e8853a911abe63727063f8d69fa3a38c2d2963
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-15 10:33:02 +0300

    update

--- includes/class-wpri-database.php
+++ includes/class-wpri-database.php
@@ -453,18 +453,18 @@ public static function update_form($id, $entity, $form) {
 
 		if (isset($entity["related"])){
 			foreach ( $entity["related"] as $related ) {
- 				if (isset($declarations[$related]["groups"][0]["elements"][0]["all_locales"])){
-					$success = $GLOBALS['wpdb']->query(
-						$GLOBALS['wpdb']->prepare(
-							"DELETE FROM " . self::table_name($related."_".$entity["name"]). " WHERE ".$entity["name"]." = %d", $id
-						)
-					);
-				} else{
-					$GLOBALS['wpdb']->prepare(
-							"DELETE FROM " . self::table_name($related."_".$entity["name"]). " WHERE ".$entity["name"]." = %d", $id
-						)
-					);
-				}
+ 			// 	if (isset($declarations[$related]["groups"][0]["elements"][0]["all_locales"])){
+				// 	$success = $GLOBALS['wpdb']->query(
+				// 		$GLOBALS['wpdb']->prepare(
+				// 			"DELETE FROM " . self::table_name($related."_".$entity["name"]). " WHERE ".$entity["name"]." = %d", $id
+				// 		)
+				// 	);
+				// } else{
+				// 	$GLOBALS['wpdb']->prepare(
+				// 			"DELETE FROM " . self::table_name($related."_".$entity["name"]). " WHERE ".$entity["name"]." = %d", $id
+				// 		)
+				// 	);
+				// }
 		}
 	}
 
