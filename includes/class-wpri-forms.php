990e53ed00c271577a55fa4ecffd6137df9d6ead
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-16 22:22:46 +0300

    add templates

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -267,7 +267,7 @@ class WPRI_Form {
 										echo "<ul id='output".$element["name"].$option["id"]."' class='list-group' style='min-height:100px'>";
                                          if ( isset($element["data"])){
                                              foreach ( $all_entries as $item ) {
-                                                 if ( $item[$relation["foreach"]["table"]]==$element["data"][$relation["foreach"]["table"]] &&  $option["id"]==$element["data"][$relation["select"]["table"]]){
+                                                 if ( $item["id"]==$element["data"][$relation["foreach"]["table"]] &&  $option["id"]==$element["data"][$relation["select"]["table"]]){
                                                    echo "<li
                                                        class='list-group-item' optionname='".$item["id"]."'>
                                                        <span class='glyphicon glyphicon-move' aria-hidden='true'></span>".	$item[$relation["foreach"]["display_column"]]."</li>";
