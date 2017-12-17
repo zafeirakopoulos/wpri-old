b5613b770bd7cdd7f618dbde4c616cacd76cffd8
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-17 20:11:28 +0300

    add templates

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -275,8 +275,6 @@ class WPRI_Form {
                                                          }
                                                     }
                                                 }
-                                            } else{
-
                                             }
 
                                          foreach ($items_to_add as $add_item) {
@@ -289,7 +287,7 @@ class WPRI_Form {
                                          foreach ($all_entries as $item) {
                                              echo "<li
                                                  class='list-group-item' optionname='".$item["id"]."'>
-                                                 <span class='glyphicon glyphicon-move' aria-hidden='true'></span>".	$item[$relation["foreach"]["display_column"]]."</li>";
+                                                 <span class='glyphicon glyphicon-move' aria-hidden='true'></span>".$item[$relation["foreach"]["display_column"]]."</li>";
                                          }
                                      }
                                 } else {
