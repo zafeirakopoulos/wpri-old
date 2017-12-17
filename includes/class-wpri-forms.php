e07c3492181348db02d1ccab8922ff2e93cc766a
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-18 00:14:35 +0300

    add templates

--- includes/class-wpri-forms.php
+++ includes/class-wpri-forms.php
@@ -248,7 +248,7 @@ class WPRI_Form {
 						<?php }
 						elseif ($element["type"]=="multiple-select"){
 							$relation = $element["relation"];
-                            echo "<div class='col-sm-12 `form-element-caption'>".$element["caption"]."</div>";
+                            echo "<div class='form-element-caption'>".$element["caption"]."</div>";
 
                             // echo  $element["caption"] ; // TODO remove
 
@@ -423,7 +423,7 @@ class WPRI_Form {
 
 						}
 						elseif ($element["type"]=="select"){
-							echo "<div class='form-element-caption'>".$element["caption"]."</div>";
+							echo "<div class='col-sm-12 form-element-caption'>".$element["caption"]."</div>";
 							echo "<select name='".$element["name"]."'>";
 							$all_entries = array();
 							foreach (WPRI_Database::get_all($element["table"]) as $dbitem) {
