ac19aec22ccb6b1e7f0f145abbc0640d39141725
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-18 17:11:42 +0300

    add templates

--- includes/wpri-session.php
+++ includes/wpri-session.php
@@ -5,16 +5,15 @@ nocache_headers();
 // ONLY USE A VARIABLE WITHIN $_SESSION (here, "ajjx")
 // OTHERWISE THIS MAY ALLOW ANYONE TO TAKE CONTROL OF YOUR INSTALLATION.
 // Check if locale is set
-error_log("about to change locale");
+// error_log("about to change locale");
 $_SESSION['locale'] = $_REQUEST['data']['locale'];
 
-error_log(plugin_dir_path( dirname( __FILE__ ) ) .'/../languages/wpri-'.WPRI_Database::get_locale($_SESSION['locale']).'.mo');
-load_textdomain( "wpri", plugin_dir_path( dirname( __FILE__ ) ) .'/../languages/wpri-'.WPRI_Database::get_locale($_SESSION['locale']).'.mo' );
+// error_log(plugin_dir_path( dirname( __FILE__ ) ) .'/../languages/wpri-'.WPRI_Database::get_locale($_SESSION['locale']).'.mo');
+// load_textdomain( "wpri", plugin_dir_path( dirname( __FILE__ ) ) .'/../languages/wpri-'.WPRI_Database::get_locale($_SESSION['locale']).'.mo' );
 
 
-Header('Content-Type: application/text;charset=utf8');
-// die(json_encode(array(
-//     'result' => "about to change locale", // This in case you want to return something to the caller
-// )));
-die("about to change locale");
- ?>
+Header('Content-Type: application/json;charset=utf8');
+die(json_encode(array(
+    'result' => 'about to change locale', // This in case you want to return something to the caller
+)));
+  ?>
