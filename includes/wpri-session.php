df656251b7e9e67478f5298382380b51af3878c5
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-18 17:12:46 +0300

    add templates

--- includes/wpri-session.php
+++ includes/wpri-session.php
@@ -11,9 +11,10 @@ $_SESSION['locale'] = $_REQUEST['data']['locale'];
 // error_log(plugin_dir_path( dirname( __FILE__ ) ) .'/../languages/wpri-'.WPRI_Database::get_locale($_SESSION['locale']).'.mo');
 // load_textdomain( "wpri", plugin_dir_path( dirname( __FILE__ ) ) .'/../languages/wpri-'.WPRI_Database::get_locale($_SESSION['locale']).'.mo' );
 
+echo "test";
 
-Header('Content-Type: application/json;charset=utf8');
-die(json_encode(array(
-    'result' => 'about to change locale', // This in case you want to return something to the caller
-)));
+// Header('Content-Type: application/json;charset=utf8');
+// die(json_encode(array(
+//     'result' => 'about to change locale', // This in case you want to return something to the caller
+// )));
   ?>
