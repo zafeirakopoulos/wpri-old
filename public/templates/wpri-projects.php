f2789e76421f82908182bd27bf636e402e01b1bc
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-18 14:54:07 +0300

    add templates

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -8,7 +8,7 @@
 				$project = WPRI_Database::get_entity_raw("project",$project_id);
                 if (in_array($project["status"], array(1,3,4))) {
                     echo "<a class='row list-group-item' href='".site_url()."/project?id=".$project_id."'>";
-                        echo "<img class='col-xs-2'  src='".$project["picture"]."' width='50px'></div>";
+                        echo "<img class='col-xs-2'  src='".$project["picture"]."' width='50px'>";
                         echo "<div class='col-xs-10 list-group-item'>";
                             echo "<h2 class='list-item'>".$project['title']."</h2>";
                             echo "<h3 class='list-item'>Funded by: ".join(",",$project['agency'])."</h3>";
