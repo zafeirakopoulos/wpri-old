2110e035f3756726758cbf8289ead3661803f8da
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-18 23:36:47 +0300

    add templates

--- public/templates/wpri-header.php
+++ public/templates/wpri-header.php
@@ -19,6 +19,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+	<?php wp_head();?>
 </head>
 
 <body>
@@ -26,7 +27,7 @@
 		if(!isset($_SESSION['locale'])) {
 		    $_SESSION['locale']=1;
 		}
-		echo get_locale();
+ 
 	?>
 
 <div class="masthead container">
