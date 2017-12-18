88aaa0a86ca70c70b9ba1a940e7b0780f57059ae
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-12-18 12:16:29 +0300

    add templates

--- public/templates/wpri-projects.php
+++ public/templates/wpri-projects.php
@@ -6,7 +6,13 @@
 			$project_ids= WPRI_Database::get_ids("project");
  			foreach ( $project_ids as $project_id ) {
 				$project = WPRI_Database::get_entity("project",$project_id);
-
+                if (in_array($project["status"], array(1,3,4))) {
+                    echo "<a class='list-group-item' href='".site_url()."/project?id=".$project_id."'>";
+                        echo "<h2 class='list-item'>".$project['title']."</h2><br>";
+                        echo "<h3 class='list-item'>Funded by: ".join(",",$project['agency'])."</h3>";
+                        // echo "<div>".$project["picture"]."</div>";
+                    echo "</a><hr/>";
+                }
                 // echo "<div class='row'>
                 //         <a href='".site_url()."/project?id=".$project_id."' class='single'>
                 //             <div class='col-xs-8 offset-xs-2 single'>
@@ -16,13 +22,9 @@
                 //             </div>
                 //         </a>
                 //         </div>
-                //     <hr/>";
+                //     ";
 
-                    echo "<a class='list-group-item' href='".site_url()."/project?id=".$project_id."'>";
-                        echo "<h2 class='list-item'>".$project['title']."</h2><br>";
-                        echo "<h3 class='list-item'>Funded by: ".join(",",$project['agency'])."</h3>";
-                        // echo "<div>".$project["picture"]."</div>";
-                    echo "</a>";
+
 			}
  			?>
         </ul>
